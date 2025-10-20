e2e605d16b79cadc83cd51647f95f09a7e764827
Teddi <teddikao@gmail.com>
2025-10-20 09:58:09 +0800

    Ensure that icons are centered horizontally

--- app/src/shared/components/DashboardNavbar.tsx
+++ app/src/shared/components/DashboardNavbar.tsx
@@ -2,7 +2,7 @@ import HomeIcon from "./icons/Home";
 
 function DashboardNavbar() {
     return (
-        <div className="w-10 flex flex-col bg-gray-100 h-screen">
+        <div className="w-10 flex flex-col items-center bg-gray-100 h-screen">
             <HomeIcon className="fill-gray-400 w-6 h-6" />
         </div>
     )
