d5ae2a792993b682a095aa6be2d94cd3df34d019
Teddi <teddikao@gmail.com>
2025-10-20 11:28:30 +0800

    Create ProfilePicture component

--- app/src/shared/components/ProfilePicture.tsx
+++ app/src/shared/components/ProfilePicture.tsx
@@ -1,7 +1,23 @@
 "use client";
 
-function ProfilePicture() {
+import { useUser } from "@clerk/nextjs";
 
+type ProfilePictureProps = {
+	className?: string;
+};
+
+function ProfilePicture({ className = "" }: ProfilePictureProps) {
+	const { user } = useUser();
+
+	if (!user) return null;
+
+	return (
+		<img
+			src={user.imageUrl}
+			alt={`${user.fullName}'s profile picture`}
+            className={className}
+		/>
+	);
 }
 
 export default ProfilePicture;
