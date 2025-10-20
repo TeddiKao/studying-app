1a5437c4af241b6c4090cb8e9bf3b6b0384b6f77
Teddi <teddikao@gmail.com>
2025-10-20 11:31:01 +0800

    Add profile picture component to dashboard navbar

--- app/src/shared/components/DashboardNavbar.tsx
+++ app/src/shared/components/DashboardNavbar.tsx
@@ -1,8 +1,13 @@
 import HomeIcon from "./icons/Home";
+import ProfilePicture from "./ProfilePicture";
 
 function DashboardNavbar() {
     return (
         <div className="w-max flex flex-col gap-3 items-center bg-gray-100 h-screen py-2 px-3">
+            <div className="flex flex-col">
+                <ProfilePicture className="w-6 h-6 rounded-md bg-gray-950" />
+            </div>
+            
             <HomeIcon className="fill-gray-950 w-5 h-5" />
         </div>
     )
