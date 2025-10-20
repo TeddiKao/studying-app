b4d86ab42c0c2a88e6a36904e95b29d63d96698c
Teddi <teddikao@gmail.com>
2025-10-20 11:57:04 +0800

    Add missing tailwind CSS file

---
+++ app/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./src/<next-js-app>/app/**/*.{js,ts,jsx,tsx}",
+		"./src/<next-js-app>/components/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
