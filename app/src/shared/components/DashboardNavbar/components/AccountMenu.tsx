46c96878f1cb2f31330fe49f7b462c4c02b94b38
Teddi <teddikao@gmail.com>
2025-10-20 13:29:54 +0800

    Add logout functionality

--- app/src/shared/components/DashboardNavbar/components/AccountMenu.tsx
+++ app/src/shared/components/DashboardNavbar/components/AccountMenu.tsx
@@ -1,10 +1,13 @@
 import { DropdownMenuContent, DropdownMenuItem } from "@/components/ui/dropdown-menu";
+import { SignOutButton } from "@clerk/nextjs";
 
 function AccountMenu() {
 	return (
         <DropdownMenuContent side="right" sideOffset={8} align="start" alignOffset={0} className="border-gray-200">
             <DropdownMenuItem>Profile</DropdownMenuItem>
-            <DropdownMenuItem>Logout</DropdownMenuItem>
+            <DropdownMenuItem>
+                <SignOutButton />
+            </DropdownMenuItem>
         </DropdownMenuContent>
     );
 }
