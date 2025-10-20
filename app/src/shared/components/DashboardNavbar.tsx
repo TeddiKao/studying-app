df9649be27252b9c783848a3e0bc36400b9c1fb8
Teddi <teddikao@gmail.com>
2025-10-20 11:32:44 +0800

    Increase size of profile picture and home icon

--- app/src/shared/components/DashboardNavbar.tsx
+++ app/src/shared/components/DashboardNavbar.tsx
@@ -3,12 +3,12 @@ import ProfilePicture from "./ProfilePicture";
 
 function DashboardNavbar() {
     return (
-        <div className="w-max flex flex-col gap-3 items-center bg-gray-100 h-screen py-2 px-3">
+        <div className="w-max flex flex-col gap-3 items-center bg-gray-100 h-screen py-2 px-4">
             <div className="flex flex-col">
-                <ProfilePicture className="w-6 h-6 rounded-md bg-gray-950" />
+                <ProfilePicture className="w-8 h-8 rounded-md bg-gray-950" />
             </div>
             
-            <HomeIcon className="fill-gray-950 w-5 h-5" />
+            <HomeIcon className="fill-gray-950 w-6 h-6" />
         </div>
     )
 }
