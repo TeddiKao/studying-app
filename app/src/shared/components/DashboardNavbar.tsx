9fc7e240c68da68005e96df42bd36307d20b4fa5
Teddi <teddikao@gmail.com>
2025-10-20 09:56:47 +0800

    Add HomeIcon to navbar

--- app/src/shared/components/DashboardNavbar.tsx
+++ app/src/shared/components/DashboardNavbar.tsx
@@ -1,7 +1,9 @@
+import HomeIcon from "./icons/Home";
+
 function DashboardNavbar() {
     return (
         <div className="w-10 flex flex-col bg-gray-100 h-screen">
-            
+            <HomeIcon className="fill-gray-400 w-6 h-6" />
         </div>
     )
 }
