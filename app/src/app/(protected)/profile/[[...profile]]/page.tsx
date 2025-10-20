cbd48b796e7f3f6be45e9c185b50ac568bda70c7
Teddi <teddikao@gmail.com>
2025-10-20 17:05:20 +0800

    Improve styling of user profile page

--- app/src/app/(protected)/profile/[[...profile]]/page.tsx
+++ app/src/app/(protected)/profile/[[...profile]]/page.tsx
@@ -1,7 +1,19 @@
 import { UserProfile } from "@clerk/nextjs";
 
 function UserProfilePage() {
-    return <UserProfile />;
+    return <UserProfile appearance={{
+        elements: {
+            rootBox: {
+                width: "100vw",
+                maxWidth: "none",
+            },
+
+            cardBox: {
+                width: "100%",
+                maxWidth: "none",
+            }
+        }
+    }} />
 }
 
 export default UserProfilePage;
