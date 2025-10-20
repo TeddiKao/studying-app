58a615a6aa9df8f22b2be14aafa52efbeeb03776
Teddi <teddikao@gmail.com>
2025-10-20 11:24:32 +0800

    Modify navbar colour

--- app/src/shared/components/DashboardNavbar.tsx
+++ app/src/shared/components/DashboardNavbar.tsx
@@ -2,7 +2,7 @@ import HomeIcon from "./icons/Home";
 
 function DashboardNavbar() {
     return (
-        <div className="w-max flex flex-col items-center bg-zinc-100 h-screen py-2 px-3">
+        <div className="w-max flex flex-col gap-3 items-center bg-gray-100 h-screen py-2 px-3">
             <HomeIcon className="fill-gray-950 w-5 h-5" />
         </div>
     )
