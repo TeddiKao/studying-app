abd6e31ddf0964a07c459a1c47f9da975962c3a1
Teddi <teddikao@gmail.com>
2025-10-20 13:00:18 +0800

    Fix tailwind paths

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: [
-		"./src/<next-js-app>/app/**/*.{js,ts,jsx,tsx}",
-		"./src/<next-js-app>/components/**/*.{js,ts,jsx,tsx}",
+		"./src/app/**/*.{js,ts,jsx,tsx}",
+		"./src/components/components/**/*.{js,ts,jsx,tsx}",
+		"./src/shared/**/*.{js,ts,jsx,tsx}",
+		"./src/features/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
 		extend: {},
