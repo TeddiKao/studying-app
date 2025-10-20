d3bee66f9ed244768151b0359e72f79d9a59411c
Teddi <teddikao@gmail.com>
2025-10-20 13:36:42 +0800

    Add icons to account menu

--- app/src/shared/components/DashboardNavbar/components/AccountMenu.tsx
+++ app/src/shared/components/DashboardNavbar/components/AccountMenu.tsx
@@ -1,11 +1,16 @@
 import { DropdownMenuContent, DropdownMenuItem } from "@/components/ui/dropdown-menu";
 import { SignOutButton } from "@clerk/nextjs";
+import { LogOutIcon, UserIcon } from "lucide-react";
 
 function AccountMenu() {
 	return (
         <DropdownMenuContent side="right" sideOffset={8} align="start" alignOffset={0} className="border-gray-200">
-            <DropdownMenuItem>Profile</DropdownMenuItem>
-            <DropdownMenuItem>
+            <DropdownMenuItem className="flex flex-row">
+                <UserIcon />
+                <p className="text-sm">Profile</p>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="flex flex-row">
+                <LogOutIcon />
                 <SignOutButton />
             </DropdownMenuItem>
         </DropdownMenuContent>
