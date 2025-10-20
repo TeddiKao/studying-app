700c25205a62c8d2e21ae5e28061d7b6309c7707
Teddi <teddikao@gmail.com>
2025-10-20 12:13:43 +0800

    Create NavIcon component

---
+++ app/src/shared/components/DashboardNavbar/components/NavIcon.tsx
@@ -0,0 +1,21 @@
+import { Tooltip, TooltipTrigger } from "@/components/ui/tooltip";
+import { TooltipContent } from "@radix-ui/react-tooltip";
+
+type NavIconProps = {
+    children: React.ReactNode;
+    tooltip: string;
+}
+
+function NavIcon({ children, tooltip }: NavIconProps) {
+    return (
+        <Tooltip>
+            <TooltipTrigger asChild>
+                {children}
+            </TooltipTrigger>
+
+            <TooltipContent>
+                <p>{tooltip}</p>
+            </TooltipContent>
+        </Tooltip>
+    )
+}
