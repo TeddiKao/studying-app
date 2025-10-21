b089f353bb481ccb78657d5ce63f52837bbbe2a5
Teddi <teddikao@gmail.com>
2025-10-21 18:02:20 +0800

    Update imports accordingly

--- app/src/app/(protected)/layout.tsx
+++ app/src/app/(protected)/layout.tsx
@@ -1,15 +1,15 @@
-import DashboardNavbar from "@/shared/components/DashboardNavbar/DashboardNavbar";
+import AppSidebar from "@/shared/components/AppSidebar/AppSidebar";
 import { auth } from "@clerk/nextjs/server";
 
 async function ProtectedLayout({ children }: { children: React.ReactNode }) {
-    await auth.protect();
+	await auth.protect();
 
-    return (
-        <div className="flex flex-row gap-4 max-md:flex-col">
-            <DashboardNavbar />
-            <div className="grow">{children}</div>
-        </div>
-    );
+	return (
+		<div className="flex flex-row gap-4 max-md:flex-col">
+			<AppSidebar />
+			<div className="grow">{children}</div>
+		</div>
+	);
 }
 
 export default ProtectedLayout;
