af54d198c2db26317883a53ed5869acb1a37144d
Teddi <teddikao@gmail.com>
2025-10-21 18:30:49 +0800

    Integrate new button for adding notebook into page header

--- app/src/features/notebooks/components/NotebookPageHeader/NotebookPageHeader.tsx
+++ app/src/features/notebooks/components/NotebookPageHeader/NotebookPageHeader.tsx
@@ -1,7 +1,10 @@
+import AddNotebookButton from "./components/AddNotebookButton";
+
 function NotebookPageHeader() {
 	return (
         <div className="flex flex-row justify-between items-center">
             <h1 className="text-2xl font-bold">Notebooks</h1>
+            <AddNotebookButton />
         </div>
     );
 }
