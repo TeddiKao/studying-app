3acfdb9f60de52042b6706708f528c43af8aed66
Teddi <teddikao@gmail.com>
2025-10-21 19:16:08 +0800

    Integrate notebook dialog component in notebooks page

--- app/src/app/(protected)/notebooks/page.tsx
+++ app/src/app/(protected)/notebooks/page.tsx
@@ -1,10 +1,15 @@
+import NotebookDialog from "@/features/notebooks/components/NotebookDialog";
 import NotebookPageHeader from "@/features/notebooks/components/NotebookPageHeader/NotebookPageHeader";
 
 function Notebooks() {
 	return (
-		<div className="flex flex-col">
-			<NotebookPageHeader />
-		</div>
+		<>
+			<div className="flex flex-col">
+				<NotebookPageHeader />
+			</div>
+
+			<NotebookDialog mode="create" />
+		</>
 	);
 }
 
