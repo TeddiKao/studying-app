e9795a83a9638941ecda5e48475ede4b9d19ecc6
Teddi <teddikao@gmail.com>
2025-10-21 18:29:41 +0800

    Create component for add notebook button

---
+++ app/src/features/notebooks/components/NotebookPageHeader/components/AddNotebookButton.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+function AddNotebookButton() {
+	return (
+		<button type="button" className="bg-gray-950 text-white px-4 py-2 rounded-md">
+			Add Notebook
+		</button>
+	);
+}
+
+export default AddNotebookButton;
