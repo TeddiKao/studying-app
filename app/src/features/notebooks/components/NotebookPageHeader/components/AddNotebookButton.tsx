76066003a15348d658c1df8c1a1b34349714601e
Teddi <teddikao@gmail.com>
2025-10-21 18:52:37 +0800

    Add plus icon to button for adding notebook

--- app/src/features/notebooks/components/NotebookPageHeader/components/AddNotebookButton.tsx
+++ app/src/features/notebooks/components/NotebookPageHeader/components/AddNotebookButton.tsx
@@ -1,9 +1,15 @@
 "use client";
 
+import PlusIcon from "@/shared/components/icons/Plus";
+
 function AddNotebookButton() {
 	return (
-		<button type="button" className="bg-gray-950 text-white px-4 py-2 rounded-md hover:cursor-pointer">
-			Add Notebook
+		<button
+			type="button"
+			className="flex flex-row gap-2 items-center bg-gray-950 text-white px-4 py-2 rounded-md hover:cursor-pointer"
+		>
+			<PlusIcon className="w-4 h-4 fill-white" />
+			<span>Add notebook</span>
 		</button>
 	);
 }
