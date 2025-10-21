09fcd48b081e0795b2cdbf060197391d10de8103
Teddi <teddikao@gmail.com>
2025-10-21 17:57:54 +0800

    Make content display vertically on smaller screens

--- app/src/app/(protected)/layout.tsx
+++ app/src/app/(protected)/layout.tsx
@@ -5,7 +5,7 @@ async function ProtectedLayout({ children }: { children: React.ReactNode }) {
     await auth.protect();
 
     return (
-        <div className="flex flex-row gap-4">
+        <div className="flex flex-row gap-4 max-md:flex-col">
             <DashboardNavbar />
             <div className="grow">{children}</div>
         </div>
