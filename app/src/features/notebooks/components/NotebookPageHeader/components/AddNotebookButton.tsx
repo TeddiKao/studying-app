d56330cca053161498cd03d6f823d5a1874eadb8
Teddi <teddikao@gmail.com>
2025-10-21 19:14:25 +0800

    Add logic to update form state to open form upon clicking

--- app/src/features/notebooks/components/NotebookPageHeader/components/AddNotebookButton.tsx
+++ app/src/features/notebooks/components/NotebookPageHeader/components/AddNotebookButton.tsx
@@ -1,11 +1,19 @@
 "use client";
 
+import { useCreateNotebookFormStore } from "@/features/notebooks/stores/createNotebookForm";
 import PlusIcon from "@/shared/components/icons/Plus";
 
 function AddNotebookButton() {
+    const { openForm } = useCreateNotebookFormStore();
+
+    function handleAddNotebookClick() {
+        openForm();
+    }
+
 	return (
 		<button
 			type="button"
+            onClick={handleAddNotebookClick}
 			className="flex flex-row gap-2 items-center bg-gray-950 text-white px-4 py-2 rounded-md hover:cursor-pointer"
 		>
 			<PlusIcon className="w-4 h-4 fill-white" />
