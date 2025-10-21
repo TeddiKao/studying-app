6abcbf88a4e398ed214f78b5d2e9490bef901143
Teddi <teddikao@gmail.com>
2025-10-21 18:33:09 +0800

    Add hover styles for add notebook button

--- app/src/features/notebooks/components/NotebookPageHeader/components/AddNotebookButton.tsx
+++ app/src/features/notebooks/components/NotebookPageHeader/components/AddNotebookButton.tsx
@@ -2,7 +2,7 @@
 
 function AddNotebookButton() {
 	return (
-		<button type="button" className="bg-gray-950 text-white px-4 py-2 rounded-md">
+		<button type="button" className="bg-gray-950 text-white px-4 py-2 rounded-md hover:cursor-pointer">
 			Add Notebook
 		</button>
 	);
