68a573dabf882b49657753f98fa631f86ad0d730
Teddi <teddikao@gmail.com>
2025-10-22 11:04:49 +0800

    Add kebab menu for additional actions

--- app/src/features/notebooks/components/NotebookCard.tsx
+++ app/src/features/notebooks/components/NotebookCard.tsx
@@ -1,8 +1,9 @@
 import NotebookIcon from "@/shared/components/icons/Notebook";
+import { EllipsisVertical } from "lucide-react";
 
 function NotebookCard() {
 	return (
-		<div className="bg-gray-50 rounded-md shadow-md p-2">
+		<div className="flex flex-row gap-2 items-center justify-between bg-gray-50 rounded-md shadow-md p-2">
 			<button type="button" className="flex flex-row gap-2 items-center">
 				<NotebookIcon className="w-6 h-6 fill-gray-500" />
 				<span className="flex flex-col gap-0.5">
@@ -10,6 +11,10 @@ function NotebookCard() {
 					<span className="text-sm text-gray-500 text-left">30 notes</span>
 				</span>
 			</button>
+
+            <button type="button">
+                <EllipsisVertical className="stroke-gray-500" />
+            </button>
 		</div>
 	);
 }
