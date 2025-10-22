0d3aa34260a25fe2e6b967a34ae803e03f6fd145
Teddi <teddikao@gmail.com>
2025-10-22 10:50:03 +0800

    Add styling for notebook card

--- app/src/features/notebooks/components/NotebookCard.tsx
+++ app/src/features/notebooks/components/NotebookCard.tsx
@@ -1,5 +1,15 @@
+import NotebookIcon from "@/shared/components/icons/Notebook";
+
 function NotebookCard() {
-    return <div>NotebookCard</div>
+    return (
+        <div className="flex flex-row gap-2 items-center bg-white shadow-sm p-2">
+            <NotebookIcon className="w-6 h-6 fill-gray-500" />
+            <div className="flex flex-col gap-0.5">
+                <h3 className="text-lg font-semibold">History notebook</h3>
+                <p className="text-sm text-gray-500">30 notes</p>
+            </div>
+        </div>
+    )
 }
 
 export default NotebookCard;
