3810533a01de947c0d91ddc8eb4e371b2b1183a0
Teddi <teddikao@gmail.com>
2025-10-22 11:10:19 +0800

    Improve formatting

--- app/src/features/notebooks/components/NotebookCard.tsx
+++ app/src/features/notebooks/components/NotebookCard.tsx
@@ -6,17 +6,24 @@ import { EllipsisVertical } from "lucide-react";
 function NotebookCard() {
 	return (
 		<div className="flex flex-row gap-2 items-center justify-between bg-gray-50 rounded-md shadow-md p-2">
-			<button type="button" className="flex flex-row gap-2 items-center hover:cursor-pointer">
+			<button
+				type="button"
+				className="flex flex-row gap-2 items-center hover:cursor-pointer"
+			>
 				<NotebookIcon className="w-6 h-6 fill-gray-500" />
 				<span className="flex flex-col gap-0.5">
-					<span className="text-lg font-semibold text-left">History notebook</span>
-					<span className="text-sm text-gray-500 text-left">30 notes</span>
+					<span className="text-lg font-semibold text-left">
+						History notebook
+					</span>
+					<span className="text-sm text-gray-500 text-left">
+						30 notes
+					</span>
 				</span>
 			</button>
 
-            <button type="button" className="hover:cursor-pointer">
-                <EllipsisVertical className="stroke-gray-500" />
-            </button>
+			<button type="button" className="hover:cursor-pointer">
+				<EllipsisVertical className="stroke-gray-500" />
+			</button>
 		</div>
 	);
 }
