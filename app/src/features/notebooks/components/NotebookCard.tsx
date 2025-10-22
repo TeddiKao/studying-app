83786b20f08bd517b686452d6154d7872e351661
Teddi <teddikao@gmail.com>
2025-10-22 10:55:37 +0800

    Darken background colour for improved contrast

--- app/src/features/notebooks/components/NotebookCard.tsx
+++ app/src/features/notebooks/components/NotebookCard.tsx
@@ -2,7 +2,7 @@ import NotebookIcon from "@/shared/components/icons/Notebook";
 
 function NotebookCard() {
 	return (
-		<div className="flex flex-row gap-2 items-center bg-white rounded-md shadow-md p-2">
+		<div className="flex flex-row gap-2 items-center bg-gray-50 rounded-md shadow-md p-2">
 			<NotebookIcon className="w-6 h-6 fill-gray-500" />
 			<div className="flex flex-col gap-0.5">
 				<h3 className="text-lg font-semibold">History notebook</h3>
