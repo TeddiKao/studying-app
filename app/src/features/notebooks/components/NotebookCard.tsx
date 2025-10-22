9340608cee38b2119a0703833bc132b17a36614a
Teddi <teddikao@gmail.com>
2025-10-22 10:59:43 +0800

    Change structure such that notebook card is a button and to handle additional dropdown menu functionality

--- app/src/features/notebooks/components/NotebookCard.tsx
+++ app/src/features/notebooks/components/NotebookCard.tsx
@@ -2,12 +2,14 @@ import NotebookIcon from "@/shared/components/icons/Notebook";
 
 function NotebookCard() {
 	return (
-		<div className="flex flex-row gap-2 items-center bg-gray-50 rounded-md shadow-md p-2">
-			<NotebookIcon className="w-6 h-6 fill-gray-500" />
-			<div className="flex flex-col gap-0.5">
-				<h3 className="text-lg font-semibold">History notebook</h3>
-				<p className="text-sm text-gray-500">30 notes</p>
-			</div>
+		<div className="bg-gray-50 rounded-md shadow-md p-2">
+			<button type="button" className="flex flex-row gap-2 items-center">
+				<NotebookIcon className="w-6 h-6 fill-gray-500" />
+				<span className="flex flex-col gap-0.5">
+					<span className="text-lg font-semibold text-left">History notebook</span>
+					<span className="text-sm text-gray-500 text-left">30 notes</span>
+				</span>
+			</button>
 		</div>
 	);
 }
