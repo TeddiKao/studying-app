7f759de0392036b7b2177147de11d7683f8d1767
Teddi <teddikao@gmail.com>
2025-10-22 10:53:54 +0800

    Improve code formatting

--- app/src/features/notebooks/components/NotebookCard.tsx
+++ app/src/features/notebooks/components/NotebookCard.tsx
@@ -1,15 +1,15 @@
 import NotebookIcon from "@/shared/components/icons/Notebook";
 
 function NotebookCard() {
-    return (
-        <div className="flex flex-row gap-2 items-center bg-white rounded-md shadow-md p-2">
-            <NotebookIcon className="w-6 h-6 fill-gray-500" />
-            <div className="flex flex-col gap-0.5">
-                <h3 className="text-lg font-semibold">History notebook</h3>
-                <p className="text-sm text-gray-500">30 notes</p>
-            </div>
-        </div>
-    )
+	return (
+		<div className="flex flex-row gap-2 items-center bg-white rounded-md shadow-md p-2">
+			<NotebookIcon className="w-6 h-6 fill-gray-500" />
+			<div className="flex flex-col gap-0.5">
+				<h3 className="text-lg font-semibold">History notebook</h3>
+				<p className="text-sm text-gray-500">30 notes</p>
+			</div>
+		</div>
+	);
 }
 
 export default NotebookCard;
