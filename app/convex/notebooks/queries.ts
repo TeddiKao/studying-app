706a40cdc8a05839ac5968bf4ef2c66d8c22a31c
Teddi <teddikao@gmail.com>
2025-10-22 15:50:27 +0800

    Update type of ownerId to allow null values

--- app/convex/notebooks/queries.ts
+++ app/convex/notebooks/queries.ts
@@ -2,8 +2,10 @@ import { v } from "convex/values";
 import { query } from "../_generated/server";
 
 const fetchNotebooks = query({
-	args: { ownerId: v.string() },
+	args: { ownerId: v.union(v.string(), v.null()) },
 	handler: async (ctx, { ownerId }) => {
+		if (!ownerId) return [];
+		
 		const notebooks = await ctx.db
 			.query("notebooks")
 			.withIndex("by_owner", (q) => q.eq("owner", ownerId))
