673642fe3a0eaa23c331d9185e80750ea32b3fcd
Teddi <teddikao@gmail.com>
2025-10-23 15:20:56 +0800

    Modify colour of delete dropdown to red

--- app/src/features/notebooks/components/NotebookDropdown.tsx
+++ app/src/features/notebooks/components/NotebookDropdown.tsx
@@ -8,7 +8,7 @@ function NotebookDropdown() {
 	return (
 		<DropdownMenuContent className="border-gray-300" side="right" sideOffset={8} align="start">
 			<DropdownMenuItem>Edit</DropdownMenuItem>
-			<DropdownMenuItem>Delete</DropdownMenuItem>
+			<DropdownMenuItem className="text-red-500">Delete</DropdownMenuItem>
 		</DropdownMenuContent>
 	);
 }
