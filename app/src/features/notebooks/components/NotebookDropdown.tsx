567b9ac819adf0a5ee25fd779173db1e71d907ed
Teddi <teddikao@gmail.com>
2025-10-23 15:53:40 +0800

    Add notebook deletion logic upon dropdown menu delete button click

--- app/src/features/notebooks/components/NotebookDropdown.tsx
+++ app/src/features/notebooks/components/NotebookDropdown.tsx
@@ -7,19 +7,22 @@ import {
 import { useMutation } from "convex/react";
 import { PencilIcon, Trash2Icon } from "lucide-react";
 import { api } from "../../../../convex/_generated/api";
+import { Id } from "../../../../convex/_generated/dataModel";
 
 type NotebookDropdownProps = {
-	notebookId: string;
+	notebookId: Id<"notebooks">;
 };
 
 function NotebookDropdown({ notebookId }: NotebookDropdownProps) {
+	const deleteNotebook = useMutation(api.notebooks.mutations.deleteNotebook);
+
 	return (
 		<DropdownMenuContent className="border-gray-300" side="right" sideOffset={8} align="start">
 			<DropdownMenuItem>
                 <PencilIcon />
                 <span className="text-sm">Edit</span>
             </DropdownMenuItem>
-			<DropdownMenuItem>
+			<DropdownMenuItem onClick={() => deleteNotebook({ notebookId: notebookId })}>
                 <Trash2Icon className="stroke-red-500" />
                 <span className="text-sm text-red-500">Delete</span>
             </DropdownMenuItem>
