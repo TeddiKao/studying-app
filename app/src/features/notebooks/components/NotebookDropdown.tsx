313a792c38f7346aed41c596ae405e05f3a5b68c
Teddi <teddikao@gmail.com>
2025-10-23 16:05:42 +0800

    Add logic to open alert on notebook dropdown

--- app/src/features/notebooks/components/NotebookDropdown.tsx
+++ app/src/features/notebooks/components/NotebookDropdown.tsx
@@ -8,13 +8,14 @@ import { useMutation } from "convex/react";
 import { PencilIcon, Trash2Icon } from "lucide-react";
 import { api } from "../../../../convex/_generated/api";
 import { Id } from "../../../../convex/_generated/dataModel";
+import { useDeleteNotebookAlertStore } from "../stores/deleteNotebookAlert";
 
 type NotebookDropdownProps = {
 	notebookId: Id<"notebooks">;
 };
 
 function NotebookDropdown({ notebookId }: NotebookDropdownProps) {
-	const deleteNotebook = useMutation(api.notebooks.mutations.deleteNotebook);
+	const { openAlert } = useDeleteNotebookAlertStore();
 
 	return (
 		<DropdownMenuContent className="border-gray-300" side="right" sideOffset={8} align="start">
@@ -22,7 +23,7 @@ function NotebookDropdown({ notebookId }: NotebookDropdownProps) {
                 <PencilIcon />
                 <span className="text-sm">Edit</span>
             </DropdownMenuItem>
-			<DropdownMenuItem onClick={() => deleteNotebook({ notebookId: notebookId })}>
+			<DropdownMenuItem onClick={openAlert}>
                 <Trash2Icon className="stroke-red-500" />
                 <span className="text-sm text-red-500">Delete</span>
             </DropdownMenuItem>
