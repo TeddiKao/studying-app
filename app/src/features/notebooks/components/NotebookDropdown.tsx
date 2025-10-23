4d5515a9f655c4e79349793fcc72a505b095b7de
Teddi <teddikao@gmail.com>
2025-10-23 15:22:47 +0800

    Add icons to dropdown items for increased visual appeal

--- app/src/features/notebooks/components/NotebookDropdown.tsx
+++ app/src/features/notebooks/components/NotebookDropdown.tsx
@@ -3,12 +3,19 @@ import {
 	DropdownMenuContent,
 	DropdownMenuItem,
 } from "@/components/ui/dropdown-menu";
+import { PencilIcon, Trash2Icon } from "lucide-react";
 
 function NotebookDropdown() {
 	return (
 		<DropdownMenuContent className="border-gray-300" side="right" sideOffset={8} align="start">
-			<DropdownMenuItem>Edit</DropdownMenuItem>
-			<DropdownMenuItem className="text-red-500">Delete</DropdownMenuItem>
+			<DropdownMenuItem>
+                <PencilIcon />
+                <span className="text-sm">Edit</span>
+            </DropdownMenuItem>
+			<DropdownMenuItem>
+                <Trash2Icon className="stroke-red-500" />
+                <span className="text-sm text-red-500">Delete</span>
+            </DropdownMenuItem>
 		</DropdownMenuContent>
 	);
 }
