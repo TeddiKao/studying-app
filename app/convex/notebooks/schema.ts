31108fa667717e696b8ad2de56c564e339e84b08
Teddi <teddikao@gmail.com>
2025-10-23 20:57:42 +0800

    Update schema for consistency

--- app/convex/notebooks/schema.ts
+++ app/convex/notebooks/schema.ts
@@ -3,7 +3,7 @@ import { v } from "convex/values";
 
 const notebooks = defineTable({
 	name: v.string(),
-	description: v.optional(v.string()),
+	description: v.string(),
 	owner: v.string(),
 	noteCount: v.number(),
 }).index("by_owner", ["owner"]);
