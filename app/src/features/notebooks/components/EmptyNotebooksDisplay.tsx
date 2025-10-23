57cd9206ed71f54d685622c4f66f733fd908fc5c
Teddi <teddikao@gmail.com>
2025-10-23 20:21:25 +0800

    Create component to display when user has not created any notebooks

--- app/src/features/notebooks/components/EmptyNotebooksDisplay.tsx
+++ app/src/features/notebooks/components/EmptyNotebooksDisplay.tsx
@@ -1,5 +1,25 @@
-function EmptyNotebooksDisplay() {
+import { Button } from "@/components/ui/button";
+import { Empty, EmptyContent, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle } from "@/components/ui/empty";
+import NotebookIcon from "@/shared/components/icons/Notebook";
 
+function EmptyNotebooksDisplay() {
+	return (
+		<Empty>
+			<EmptyHeader>
+				<EmptyMedia variant="icon">
+					<NotebookIcon />
+				</EmptyMedia>
+                <EmptyTitle>No notebooks yet</EmptyTitle>
+                <EmptyDescription>You do not have any notebooks yet. Get started by creating one.</EmptyDescription>
+			</EmptyHeader>
+
+            <EmptyContent>
+                <div className="flex flex-row gap-2">
+                    <Button type="button">Create notebook</Button>
+                </div>
+            </EmptyContent>
+		</Empty>
+	);
 }
 
 export default EmptyNotebooksDisplay;
