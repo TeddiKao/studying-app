dcfcda0d0406f148349525de4c1dec01be200113
Teddi <teddikao@gmail.com>
2025-10-23 20:51:50 +0800

    Add notebook ID state tracking logic to alert store

--- app/src/features/notebooks/stores/deleteNotebookAlert.ts
+++ app/src/features/notebooks/stores/deleteNotebookAlert.ts
@@ -1,25 +1,34 @@
 import { create } from "zustand";
+import { Id } from "../../../../convex/_generated/dataModel";
 
 type DeleteNotebookAlertState = {
     isOpen: boolean;
     isDeleting: boolean;
+    notebookId: Id<"notebooks"> | null;
 
     openAlert: () => void;
     closeAlert: () => void;
 
     startDeleting: () => void;
     stopDeleting: () => void;
+
+    updateNotebookId: (notebookId: Id<"notebooks">) => void;
+    clearNotebookId: () => void;
 }
 
 const useDeleteNotebookAlertStore = create<DeleteNotebookAlertState>((set) => ({
     isOpen: false,
     isDeleting: false,
+    notebookId: null,
 
     openAlert: () => set({ isOpen: true }),
     closeAlert: () => set({ isOpen: false }),
 
     startDeleting: () => set({ isDeleting: true }),
     stopDeleting: () => set({ isDeleting: false }),
+
+    updateNotebookId: (notebookId: Id<"notebooks">) => set({ notebookId }),
+    clearNotebookId: () => set({ notebookId: null }),
 }));
 
 export { useDeleteNotebookAlertStore };
