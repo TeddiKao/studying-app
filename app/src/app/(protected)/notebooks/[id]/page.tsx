39daf6f53c6d2cb45b8773c90436fe963ed694fb
Teddi <teddikao@gmail.com>
2025-10-24 19:41:55 +0800

    Integrate header component with notes page

---
+++ app/src/app/(protected)/notebooks/[id]/page.tsx
@@ -0,0 +1,9 @@
+import NotesPageHeader from "@/features/notes/components/NotesPageHeader/NotesPageHeader";
+
+function NotesPage() {
+    return (
+        <div className="flex flex-col gap-3">
+            <NotesPageHeader />
+        </div>
+    )
+}
