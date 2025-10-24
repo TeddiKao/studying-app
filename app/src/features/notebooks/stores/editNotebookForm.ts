a0e2d4eea2bfdc8184f144863adc4f1472d00963
Teddi <teddikao@gmail.com>
2025-10-24 18:00:35 +0800

    Apply similar change to edit notebook form

--- app/src/features/notebooks/stores/editNotebookForm.ts
+++ app/src/features/notebooks/stores/editNotebookForm.ts
@@ -2,50 +2,55 @@ import { Id } from "@convex/_generated/dataModel";
 import { create } from "zustand";
 
 type EditNotebookFormState = {
-    isOpen: boolean;
-    isSubmitting: boolean;
-    
-    name: string;
-    description: string;
-    notebookId: Id<"notebooks"> | null;
+	isOpen: boolean;
+	isSubmitting: boolean;
 
-    openForm: () => void;
-    closeForm: () => void;
+	name: string;
+	description: string;
+	notebookId: Id<"notebooks"> | null;
 
-    updateName: (name: string) => void;
-    updateDescription: (description: string) => void;
-    clearName: () => void;
-    clearDescription: () => void;
+	openForm: () => void;
+	closeForm: () => void;
 
-    startSubmitting: () => void;
-    stopSubmitting: () => void;
+	updateName: (name: string) => void;
+	updateDescription: (description: string) => void;
+	clearName: () => void;
+	clearDescription: () => void;
 
-    updateNotebookId: (notebookId: Id<"notebooks">) => void;
-    clearNotebookId: () => void;
-}
+	startSubmitting: () => void;
+	stopSubmitting: () => void;
+
+	updateNotebookId: (notebookId: Id<"notebooks">) => void;
+	clearNotebookId: () => void;
+
+	performFormCleanup: () => void;
+};
 
 const useEditNotebookFormStore = create<EditNotebookFormState>((set) => ({
-    isOpen: false,
-    isSubmitting: false,
+	isOpen: false,
+	isSubmitting: false,
 
-    notebookId: null,
-    name: "",
-    description: "",
+	notebookId: null,
+	name: "",
+	description: "",
 
-    updateName: (name: string) => set({ name }),
-    updateDescription: (description: string) => set({ description }),
+	updateName: (name: string) => set({ name }),
+	updateDescription: (description: string) => set({ description }),
 
-    clearName: () => set({ name: "" }),
-    clearDescription: () => set({ description: "" }),
+	clearName: () => set({ name: "" }),
+	clearDescription: () => set({ description: "" }),
 
-    openForm: () => set({ isOpen: true }),
-    closeForm: () => set({ isOpen: false }),
+	openForm: () => set({ isOpen: true }),
+	closeForm: () => set({ isOpen: false }),
 
-    startSubmitting: () => set({ isSubmitting: true }),
-    stopSubmitting: () => set({ isSubmitting: false }),
+	startSubmitting: () => set({ isSubmitting: true }),
+	stopSubmitting: () => set({ isSubmitting: false }),
 
-    updateNotebookId: (notebookId: Id<"notebooks">) => set({ notebookId }),
-    clearNotebookId: () => set({ notebookId: null }),
+	updateNotebookId: (notebookId: Id<"notebooks">) => set({ notebookId }),
+	clearNotebookId: () => set({ notebookId: null }),
+
+	performFormCleanup: () =>
+		set({ name: "", description: "", notebookId: null, isOpen: false }),
 }));
 
 export { useEditNotebookFormStore };
