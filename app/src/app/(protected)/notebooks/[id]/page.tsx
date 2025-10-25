9458d203fe20933c067ec7cc153002588ed11a91
Teddi <teddikao@gmail.com>
2025-10-25 14:59:43 +0800

    Add logic to display note cards based on fetched notebooks

--- app/src/app/(protected)/notebooks/[id]/page.tsx
+++ app/src/app/(protected)/notebooks/[id]/page.tsx
@@ -1,3 +1,4 @@
+import NoteCard from "@/features/notes/components/NoteCard";
 import NoteForm from "@/features/notes/components/NoteForm";
 import NotesPageHeader from "@/features/notes/components/NotesPageHeader/NotesPageHeader";
 import { api } from "@convex/_generated/api";
@@ -11,16 +12,22 @@ type NotesPageProps = {
 async function NotesPage({ params }: NotesPageProps) {
 	const { id } = await params;
 
-	const fetchNotebooks = useQuery(api.notes.queries.fetchNotes, {
+	const notes = useQuery(api.notes.queries.fetchNotes, {
 		notebookId: id,
-	})
+	});
+
+	if (!notes) return null;
 
 	return (
 		<>
 			<div className="flex flex-col gap-3">
 				<NotesPageHeader />
 
-				<div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5"></div>
+				<div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+					{notes.map((note) => (
+						<NoteCard key={note._id} noteName={note.name} />
+					))}
+				</div>
 			</div>
 
 			<NoteForm mode="create" noteId={null} />
