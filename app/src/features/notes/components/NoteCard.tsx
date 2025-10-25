90274d4797548caa43bfed59e6b6bb24da4e751b
Teddi <teddikao@gmail.com>
2025-10-25 09:34:51 +0800

    Integrate note icon with note card

--- app/src/features/notes/components/NoteCard.tsx
+++ app/src/features/notes/components/NoteCard.tsx
@@ -1,5 +1,21 @@
-function NoteCard() {
+import NoteIcon from "@/shared/components/icons/Note";
 
+function NoteCard() {
+    return (
+        <div className="flex flex-row items-center bg-gray-50 rounded-md shadow-md p-2">
+			<button
+				type="button"
+				className="flex flex-row gap-2 items-center hover:cursor-pointer grow"
+			>
+				<NoteIcon className="w-6 h-6 stroke-gray-500" />
+				<span className="flex flex-col gap-0.5 text-left">
+					<span className="text-lg font-semibold text-left">
+						Chapter 5
+					</span>
+				</span>
+			</button>
+        </div>
+    )
 }
 
 export default NoteCard;
