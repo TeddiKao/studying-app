021834d8414e1c6a60e35ff7e5ddfa688766540e
Teddi <teddikao@gmail.com>
2025-10-25 09:56:01 +0800

    Add aria-labels for accessiblity

--- app/src/features/notes/components/NoteCard.tsx
+++ app/src/features/notes/components/NoteCard.tsx
@@ -10,6 +10,7 @@ function NoteCard({ noteName }: NoteCardProps) {
 		<div className="flex flex-row items-center bg-gray-50 rounded-md shadow-md p-2">
 			<button
 				type="button"
+                aria-label={`Open note ${noteName}`}
 				className="flex flex-row gap-2 items-center hover:cursor-pointer grow"
 			>
 				<NoteIcon className="w-6 h-6 stroke-gray-500" />
@@ -22,6 +23,7 @@ function NoteCard({ noteName }: NoteCardProps) {
 
 			<button
 				type="button"
+                aria-label={`Options for note ${noteName}`}
 				className="hover:cursor-pointer hover:bg-gray-300 rounded-md py-1"
 			>
 				<EllipsisVertical className="stroke-gray-500" />
