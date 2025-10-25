b3767705a7b3247b4c14770b1af774661634485f
Teddi <teddikao@gmail.com>
2025-10-25 14:53:25 +0800

    Create query function for fetching notes

---
+++ app/convex/notes/queries.ts
@@ -0,0 +1,29 @@
+import { v } from "convex/values";
+import { query } from "../_generated/server";
+
+const fetchNotes = query({
+    args: {
+        notebookId: v.id("notebooks")
+    },
+
+    handler: async (ctx, { notebookId }) => {
+        const userIdentity = await ctx.auth.getUserIdentity();
+        if (!userIdentity) {
+            throw new Error("Not authenticated");
+        }
+
+        const requesterId = userIdentity.subject;
+        if (requesterId !== notebookId) {
+            throw new Error("You do not have permission to view this notebook");
+        }
+
+        const notes = await ctx.db
+            .query("notes")
+            .withIndex("by_notebook_id", (q) => q.eq("notebookId", notebookId))
+            .collect();
+
+        return notes;
+    }
+})
+
+export { fetchNotes }
