9c4cd894bba26d1ff6ee6369314bee451c0326b8
Teddi <teddikao@gmail.com>
2025-10-25 20:02:38 +0800

    Create store to manage edit note form state

---
+++ app/src/features/notes/stores/editNoteForm.ts
@@ -0,0 +1,59 @@
+import { Id } from "@convex/_generated/dataModel";
+import { create } from "zustand";
+
+type EditNoteFormState = {
+    isOpen: boolean;
+	isSubmitting: boolean;
+
+	name: string;
+	description: string;
+
+    noteId: Id<"notes"> | null;
+
+	openForm: () => void;
+	closeForm: () => void;
+
+	updateName: (name: string) => void;
+	updateDescription: (description: string) => void;
+
+	clearName: () => void;
+	clearDescription: () => void;
+
+	startSubmitting: () => void;
+	stopSubmitting: () => void;
+
+	performFormCleanup: () => void;
+
+    updateNoteId: (noteId: Id<"notes">) => void;
+    clearNoteId: () => void;
+};
+
+const useEditNoteFormStore = create<EditNoteFormState>((set) => ({
+    isOpen: false,
+	isSubmitting: false,
+
+	name: "",
+	description: "",
+
+    noteId: null,
+
+    openForm: () => set({ isOpen: true }),
+    closeForm: () => set({ isOpen: false }),
+
+	updateName: (name: string) => set({ name }),
+	updateDescription: (description: string) => set({ description }),
+
+	clearName: () => set({ name: "" }),
+	clearDescription: () => set({ description: "" }),
+
+	startSubmitting: () => set({ isSubmitting: true }),
+	stopSubmitting: () => set({ isSubmitting: false }),
+
+	performFormCleanup: () =>
+		set({ isOpen: false, isSubmitting: false, name: "", description: "" }),
+
+    updateNoteId: (noteId: Id<"notes">) => set({ noteId }),
+    clearNoteId: () => set({ noteId: null }),
+}))
+
+export { useEditNoteFormStore }
