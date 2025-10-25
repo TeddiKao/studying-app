7d6a569284cb190804811bd8296fa93b0a93bb88
Teddi <teddikao@gmail.com>
2025-10-25 10:05:16 +0800

    Modify grid to be responsive

--- app/src/app/(protected)/notebooks/[id]/page.tsx
+++ app/src/app/(protected)/notebooks/[id]/page.tsx
@@ -5,7 +5,7 @@ function NotesPage() {
 		<div className="flex flex-col gap-3">
 			<NotesPageHeader />
 
-			<div className="grid grid-cols-4 gap-4"></div>
+			<div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5"></div>
 		</div>
 	);
 }
