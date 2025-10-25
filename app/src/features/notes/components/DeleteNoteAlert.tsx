b965df822759ddc6c1807a08d1d62acf9a2db538
Teddi <teddikao@gmail.com>
2025-10-25 16:38:20 +0800

    Create component for delete note alert

---
+++ app/src/features/notes/components/DeleteNoteAlert.tsx
@@ -0,0 +1,53 @@
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogDescription, AlertDialogTitle, AlertDialogHeader, AlertDialogFooter, AlertDialogContent } from "@/components/ui/alert-dialog";
+import { useDeleteNoteAlertStore } from "../stores/deleteNoteAlert";
+import { Spinner } from "@/components/ui/spinner";
+
+function DeleteNoteAlert() {
+    const { isOpen, isDeleting, openAlert, closeAlert } = useDeleteNoteAlertStore();
+
+	return (
+		<AlertDialog
+			open={isOpen}
+			onOpenChange={(open) => {
+				if (open) {
+					openAlert();
+				} else {
+					closeAlert();
+				}
+			}}
+		>
+			<AlertDialogContent>
+				<AlertDialogHeader>
+					<AlertDialogTitle>Delete notebook?</AlertDialogTitle>
+					<AlertDialogDescription>
+						Are you sure you want to delete this notebook?
+					</AlertDialogDescription>
+				</AlertDialogHeader>
+
+				<AlertDialogFooter>
+					<AlertDialogCancel>Cancel</AlertDialogCancel>
+					{isDeleting ? (
+						<AlertDialogAction
+							disabled={true}
+							aria-disabled={true}
+							className="bg-red-500 text-white opacity-75 hover:cursor-not-allowed"
+						>
+							<Spinner />
+							<span className="text-sm">Deleting ...</span>
+						</AlertDialogAction>
+					) : (
+						<AlertDialogAction
+							disabled={false}
+							aria-disabled={false}
+							className="bg-red-500 text-white hover:cursor-pointer"
+						>
+							<span className="text-sm">Delete</span>
+						</AlertDialogAction>
+					)}
+				</AlertDialogFooter>
+			</AlertDialogContent>
+		</AlertDialog>
+	);
+}
+
+export default DeleteNoteAlert;
