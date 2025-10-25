bcf35e5759e4047c3a698af34555bf16994fe7f6
Teddi <teddikao@gmail.com>
2025-10-25 16:48:45 +0800

    Integrate deletion mutation with note alert

--- app/src/features/notes/components/DeleteNoteAlert.tsx
+++ app/src/features/notes/components/DeleteNoteAlert.tsx
@@ -3,9 +3,33 @@
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogDescription, AlertDialogTitle, AlertDialogHeader, AlertDialogFooter, AlertDialogContent } from "@/components/ui/alert-dialog";
 import { useDeleteNoteAlertStore } from "../stores/deleteNoteAlert";
 import { Spinner } from "@/components/ui/spinner";
+import { api } from "@convex/_generated/api";
+import { Id } from "@convex/_generated/dataModel";
+import { useMutation } from "convex/react";
 
-function DeleteNoteAlert() {
-    const { isOpen, isDeleting, openAlert, closeAlert } = useDeleteNoteAlertStore();
+type DeleteNoteAlertProps = {
+    noteId: Id<"notes"> | null;
+}
+
+function DeleteNoteAlert({ noteId }: DeleteNoteAlertProps) {
+    const { isOpen, isDeleting, openAlert, closeAlert, startDeleting, stopDeleting } = useDeleteNoteAlertStore();
+
+    const deleteNote = useMutation(api.notes.mutations.deleteNote);
+
+    async function handleDelete() {
+        if (!noteId) return;
+
+        startDeleting();
+
+        try {
+            await deleteNote({ noteId });
+            closeAlert();
+        } catch (error) {
+            console.error(error);
+        } finally {
+            stopDeleting();
+        }
+    }
 
 	return (
 		<AlertDialog
@@ -32,6 +56,7 @@ function DeleteNoteAlert() {
 						<AlertDialogAction
 							disabled={true}
 							aria-disabled={true}
+                            onClick={handleDelete}
 							className="bg-red-500 text-white opacity-75 hover:cursor-not-allowed"
 						>
 							<Spinner />
@@ -41,6 +66,7 @@ function DeleteNoteAlert() {
 						<AlertDialogAction
 							disabled={false}
 							aria-disabled={false}
+                            onClick={handleDelete}
 							className="bg-red-500 text-white hover:cursor-pointer"
 						>
 							<span className="text-sm">Delete</span>
