5ecdba28a9f84a3e0d66526487121b7ac1fcdee4
Teddi <teddikao@gmail.com>
2025-10-26 11:51:14 +0800

    Update response types for create note mutation

--- app/convex/notes/mutations.ts
+++ app/convex/notes/mutations.ts
@@ -1,114 +1,134 @@
 import { v } from "convex/values";
 import { mutation } from "../_generated/server";
+import { ErrorResponse } from "../../src/shared/types/api";
+import { Id } from "../_generated/dataModel";
 
 const createNote = mutation({
-    args: {
-        notebookId: v.id("notebooks"),
-        title: v.string(),
-        description: v.string(),
-    },
+	args: {
+		notebookId: v.id("notebooks"),
+		title: v.string(),
+		description: v.string(),
+	},
 
-    handler: async (ctx, { notebookId, title, description }) => {
-        const userIdentity = await ctx.auth.getUserIdentity();
-        if (!userIdentity) {
-            throw new Error("Not authenticated");
-        }
+	handler: async (
+		ctx,
+		{ notebookId, title, description }
+	): Promise<ErrorResponse | Id<"notes">> => {
+		const userIdentity = await ctx.auth.getUserIdentity();
+		if (!userIdentity) {
+			throw new Error("Not authenticated");
+		}
 
-        const requesterId = userIdentity.subject;
-        const notebook = await ctx.db.get(notebookId);
+		const requesterId = userIdentity.subject;
+		const notebook = await ctx.db.get(notebookId);
 
-        if (!notebook) {
-            throw new Error("Notebook not found");
-        }
+		if (!notebook) {
+			throw new Error("Notebook not found");
+		}
 
-        const notebookOwner = notebook.owner;
-        if (notebookOwner !== requesterId) {
-            throw new Error("You do not have permission to create notes in this notebook");
-        }
+		const notebookOwner = notebook.owner;
+		if (notebookOwner !== requesterId) {
+			throw new Error(
+				"You do not have permission to create notes in this notebook"
+			);
+		}
 
-        const newNoteId = await ctx.db.insert("notes", {
-            title,
-            description,
-            notebookId,
-        });
+		const existingNotes = await ctx.db
+			.query("notes")
+			.withIndex("by_notebook_id_and_title", (q) =>
+				q.eq("notebookId", notebookId).eq("title", title)
+			)
+			.collect();
 
-        await ctx.db.patch(notebookId, {
-            noteCount: notebook.noteCount + 1,
-        })
+		if (existingNotes.length > 0) {
+			return {};
+		}
 
-        return newNoteId;
-    }
+		const newNoteId = await ctx.db.insert("notes", {
+			title,
+			description,
+			notebookId,
+		});
+
+		await ctx.db.patch(notebookId, {
+			noteCount: notebook.noteCount + 1,
+		});
+
+		return newNoteId;
+	},
 });
 
 const editNote = mutation({
-    args: {
-        noteId: v.id("notes"),
-        title: v.string(),
-        description: v.string(),
-    },
+	args: {
+		noteId: v.id("notes"),
+		title: v.string(),
+		description: v.string(),
+	},
 
-    handler: async (ctx, { noteId, title, description }) => {
-        const userIdentity = await ctx.auth.getUserIdentity();
-        if (!userIdentity) {
-            throw new Error("Not authenticated");
-        }
+	handler: async (ctx, { noteId, title, description }) => {
+		const userIdentity = await ctx.auth.getUserIdentity();
+		if (!userIdentity) {
+			throw new Error("Not authenticated");
+		}
 
-        const noteToEdit = await ctx.db.get(noteId);
-        if (!noteToEdit) {
-            throw new Error("Note not found");
-        }
+		const noteToEdit = await ctx.db.get(noteId);
+		if (!noteToEdit) {
+			throw new Error("Note not found");
+		}
 
-        const notebook = await ctx.db.get(noteToEdit.notebookId);
-        if (!notebook) {
-            throw new Error("Notebook not found");
-        }
+		const notebook = await ctx.db.get(noteToEdit.notebookId);
+		if (!notebook) {
+			throw new Error("Notebook not found");
+		}
 
-        const notebookOwner = notebook.owner;
-        if (notebookOwner !== userIdentity.subject) {
-            throw new Error("You do not have permission to edit this note");
-        }
+		const notebookOwner = notebook.owner;
+		if (notebookOwner !== userIdentity.subject) {
+			throw new Error("You do not have permission to edit this note");
+		}
 
-        await ctx.db.patch(noteId, {
-            title,
-            description,
-        });
-    }
-})
+		await ctx.db.patch(noteId, {
+			title,
+			description,
+		});
+	},
+});
 
 const deleteNote = mutation({
-    args: {
-        noteId: v.id("notes"),
-    },
+	args: {
+		noteId: v.id("notes"),
+	},
 
-    handler: async (ctx, { noteId }) => {
-        const userIdentity = await ctx.auth.getUserIdentity();
-        if (!userIdentity) {
-            throw new Error("Not authenticated");
-        }
+	handler: async (ctx, { noteId }) => {
+		const userIdentity = await ctx.auth.getUserIdentity();
+		if (!userIdentity) {
+			throw new Error("Not authenticated");
+		}
 
-        const requesterId = userIdentity.subject;
-        const note = await ctx.db.get(noteId);
+		const requesterId = userIdentity.subject;
+		const note = await ctx.db.get(noteId);
 
-        if (!note) {
-            throw new Error("Note not found");
-        }
+		if (!note) {
+			throw new Error("Note not found");
+		}
 
-        const notebook = await ctx.db.get(note.notebookId);
-        if (!notebook) {
-            throw new Error("Notebook not found");
-        }
+		const notebook = await ctx.db.get(note.notebookId);
+		if (!notebook) {
+			throw new Error("Notebook not found");
+		}
 
-        const notebookOwner = notebook.owner;
-        if (notebookOwner !== requesterId) {
-            throw new Error("You do not have permission to delete notes from this notebook");
-        }
+		const notebookOwner = notebook.owner;
+		if (notebookOwner !== requesterId) {
+			throw new Error(
+				"You do not have permission to delete notes from this notebook"
+			);
+		}
 
-        await ctx.db.delete(noteId);
+		await ctx.db.delete(noteId);
 
-        await ctx.db.patch(note.notebookId, {
-            noteCount: notebook.noteCount - 1,
-        })
-    }
-})
+		await ctx.db.patch(note.notebookId, {
+			noteCount: notebook.noteCount - 1,
+		});
+	},
+});
 
-export { createNote, editNote, deleteNote }
+export { createNote, editNote, deleteNote };
