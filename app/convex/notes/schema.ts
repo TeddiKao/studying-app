da2f6029c525fc48627e9e43f7782177e7691caf
Teddi <teddikao@gmail.com>
2025-10-26 11:38:41 +0800

    Create indexes to filter by both owner and name/title

--- app/convex/notes/schema.ts
+++ app/convex/notes/schema.ts
@@ -2,9 +2,11 @@ import { defineTable } from "convex/server";
 import { v } from "convex/values";
 
 const notes = defineTable({
-    title: v.string(),
-    description: v.string(),
-    notebookId: v.id("notebooks"),
-}).index("by_notebook_id", ["notebookId"]);
+	title: v.string(),
+	description: v.string(),
+	notebookId: v.id("notebooks"),
+})
+	.index("by_notebook_id", ["notebookId"])
+	.index("by_notebook_id_and_title", ["notebookId", "title"]);
 
 export default notes;
