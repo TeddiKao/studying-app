86a1d6e2230a7172b95f0ea90195d62d883505ae
Teddi <teddikao@gmail.com>
2025-10-26 10:23:45 +0800

    Temporarily loosen schema

--- app/convex/notes/schema.ts
+++ app/convex/notes/schema.ts
@@ -2,7 +2,8 @@ import { defineTable } from "convex/server";
 import { v } from "convex/values";
 
 const notes = defineTable({
-    title: v.string(),
+    title: v.optional(v.string()),
+    name: v.optional(v.string()),
     description: v.string(),
     notebookId: v.id("notebooks"),
 }).index("by_notebook_id", ["notebookId"]);
