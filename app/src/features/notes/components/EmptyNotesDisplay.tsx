e95161be0580aea83c77435357a15dd7fc03bec0
Teddi <teddikao@gmail.com>
2025-10-26 09:39:37 +0800

    Create component to show when no notes have been created

---
+++ app/src/features/notes/components/EmptyNotesDisplay.tsx
@@ -0,0 +1,37 @@
+import { Button } from "@/components/ui/button";
+import { Empty, EmptyContent, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle } from "@/components/ui/empty";
+import { NotebookIcon } from "lucide-react";
+import { useCreateNoteFormStore } from "../stores/createNoteForm";
+
+function EmptyNoteDisplay() {
+    const { openForm } = useCreateNoteFormStore();
+
+	return (
+		<Empty> 
+			<EmptyHeader>
+				<EmptyMedia variant="icon">
+					<NotebookIcon className="fill-gray-950" />
+				</EmptyMedia>
+				<EmptyTitle>No notes yet</EmptyTitle>
+				<EmptyDescription>
+					You do not have any notes yet. Get started by creating
+					one.
+				</EmptyDescription>
+			</EmptyHeader>
+
+			<EmptyContent>
+				<div className="flex flex-row gap-2">
+					<Button
+						onClick={openForm}
+						className="hover:cursor-pointer"
+						type="button"
+					>
+						Create notes
+					</Button>
+				</div>
+			</EmptyContent>
+		</Empty>
+	);
+}
+
+export default EmptyNoteDisplay;
