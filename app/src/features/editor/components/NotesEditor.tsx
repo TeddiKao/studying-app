b7b55568dddab0902d8c6aebdcafd667b4990c55
Teddi <teddikao@gmail.com>
2025-10-26 18:48:34 +0800

    Add extensions and styling to notes editor

--- app/src/features/editor/components/NotesEditor.tsx
+++ app/src/features/editor/components/NotesEditor.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import { EditorContent, useEditor } from "@tiptap/react";
+import { Document } from "@tiptap/extension-document";
+import { Text } from "@tiptap/extension-text";
+import { Paragraph } from "@tiptap/extension-paragraph";
 
 function NotesEditor() {
     const editor = useEditor({
-        extensions: [],
+        extensions: [Document, Text, Paragraph],
         immediatelyRender: false
     })
 
-    return <EditorContent editor={editor} />
+    return <EditorContent className="ml-12 mt-16" editor={editor} />
 }
 
 export default NotesEditor;
