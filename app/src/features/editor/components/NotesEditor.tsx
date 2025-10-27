6fb272adcc25584b9bcdaa7298a868de2c5ca081
Teddi <teddikao@gmail.com>
2025-10-27 16:49:58 +0800

    Create store to manage editor state

--- app/src/features/editor/components/NotesEditor.tsx
+++ app/src/features/editor/components/NotesEditor.tsx
@@ -5,12 +5,23 @@ import { Document } from "@tiptap/extension-document";
 import { Text } from "@tiptap/extension-text";
 import { Paragraph } from "@tiptap/extension-paragraph";
 import { Title } from "../extensions/nodes/Title";
+import { useEffect } from "react";
+import { useQuery } from "convex/react";
+import { api } from "@convex/_generated/api";
 
 function NotesEditor() {
 	const editor = useEditor({
 		extensions: [Document, Text, Paragraph, Title],
 		immediatelyRender: false,
 	});
+
+	const blocks = useQuery(api.blocks.queries.fetchBlocks, {
+		noteId: 
+	});
+
+	useEffect(() => {
+
+	} []);
 
 	return <EditorContent className="ml-12 mt-16" editor={editor} />;
 }
