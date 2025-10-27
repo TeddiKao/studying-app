9271d1d7a64378ee5daf8f006530dcba0ddd9b5e
Teddi <teddikao@gmail.com>
2025-10-27 19:22:35 +0800

    Integrate block updating with notes editor

--- app/src/features/editor/components/NotesEditor.tsx
+++ app/src/features/editor/components/NotesEditor.tsx
@@ -6,18 +6,29 @@ import { Text } from "@tiptap/extension-text";
 import { Paragraph } from "@tiptap/extension-paragraph";
 import { Title } from "../extensions/nodes/Title";
 import { useEffect } from "react";
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import { api } from "@convex/_generated/api";
 import { Id } from "@convex/_generated/dataModel";
+import { getEditorSelection } from "../utils/utils";
 
 type NotesEditorProps = {
 	noteId: Id<"notes">;
 };
 
 function NotesEditor({ noteId }: NotesEditorProps) {
+	const updateBlock = useMutation(api.blocks.mutations.updateBlock);
 	const editor = useEditor({
 		extensions: [Document, Text, Paragraph, Title],
 		immediatelyRender: false,
+
+		onSelectionUpdate: ({ editor }) => {
+			const selectedNode = getEditorSelection(editor);
+
+			updateBlock({
+				id: selectedNode.attrs.id,
+				content: selectedNode.content.toJSON(),
+			})
+		}
 	});
 
 	const blocks = useQuery(api.blocks.queries.fetchBlocks, { noteId });
