bdd7771255f4b69a284e3c6e457152d0a1fb3b19
Teddi <teddikao@gmail.com>
2025-10-29 14:47:37 +0800

    Create endpoint for saving notes

---
+++ app/src/app/api/saveBlocks/route.ts
@@ -0,0 +1,12 @@
+import { fetchMutation } from "convex/nextjs";
+import { api } from "@convex/_generated/api";
+
+async function POST(request: Request) {
+    const requestBody = await request.json();
+
+    await fetchMutation(api.blocks.mutations.bulkUdpateBlocks, {
+        blocks: requestBody.blocks
+    })
+
+    return Response.json({ success: true });
+}
