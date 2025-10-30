dfc61eba9cfd0b5239d59c033144c674a10144c0
Teddi <teddikao@gmail.com>
2025-10-30 19:35:13 +0800

    Consolidate block-related types in one file

---
+++ app/src/features/editor/types/blocks.ts
@@ -0,0 +1,23 @@
+import { Id } from "@convex/_generated/dataModel";
+import { JSONContent } from "@tiptap/react";
+
+type DBBlock = {
+	id: Id<"blocks">;
+	position: number;
+	type: string;
+	content: JSONContent[];
+	additionalAttributes: Record<string, unknown>;
+};
+
+type KnownAttrs = {
+	id: Id<"blocks">;
+	position: number;
+};
+
+type TiptapJSONBlock = {
+    type: string;
+    content: JSONContent[];
+    attrs: KnownAttrs & Record<string, unknown>;
+}
+
+export type { DBBlock, TiptapJSONBlock };
