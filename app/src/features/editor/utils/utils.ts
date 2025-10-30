cbe8cef6735f96d423da38f65de9de6519008e6a
Teddi <teddikao@gmail.com>
2025-10-30 16:35:58 +0800

    Add logic to return position in utility function

--- app/src/features/editor/utils/utils.ts
+++ app/src/features/editor/utils/utils.ts
@@ -3,37 +3,42 @@ import { Node } from "@tiptap/pm/model";
 import { Editor } from "@tiptap/react";
 
 function getEditorSelection(editor: Editor) {
-    const { state } = editor;
-    const { selection } = state;
-    const { $from } = selection;
+	const { state } = editor;
+	const { selection } = state;
+	const { $from } = selection;
 
-    const node = $from.node();
+	const node = $from.node();
 
-    return node;
+	return node;
 }
 
 function isCursorAtStartOfNode(editor: Editor) {
-    const { state } = editor;
-    const { selection } = state;
-    const { $from } = selection;
+	const { state } = editor;
+	const { selection } = state;
+	const { $from } = selection;
 
-    return $from.parentOffset === 0;
+	return $from.parentOffset === 0;
 }
 
-function getNodeFromId(editor: Editor, id: Id<"blocks">): Node | null {
-    let targetNode = null;
+function getNodeFromId(
+	editor: Editor,
+	id: Id<"blocks">
+): { targetNode: Node | null; targetPos: number | null } {
+	let targetNode = null;
+	let targetPos = null;
 
-    editor.state.doc.descendants((node, pos) => {
-        if (!node.type.isBlock) return;
+	editor.state.doc.descendants((node, pos) => {
+		if (!node.type.isBlock) return;
 
-        if (node.attrs.id === id) {
-            targetNode = node;
+		if (node.attrs.id === id) {
+			targetNode = node;
+			targetPos = pos;
 
-            return
-        }
-    });
+			return;
+		}
+	});
 
-    return targetNode;
+	return { targetNode, targetPos };
 }
 
 export { getEditorSelection, isCursorAtStartOfNode, getNodeFromId };
