97e8bd93242b75b597f74172fcd6c2763302dc86
Teddi <teddikao@gmail.com>
2025-11-01 09:48:42 +0800

    Create placeholder apply function for state

--- app/src/features/editor/extensions/plugins/transactionBatchPlugin.ts
+++ app/src/features/editor/extensions/plugins/transactionBatchPlugin.ts
@@ -1,9 +1,20 @@
 import { Extension } from "@tiptap/react";
 import { Plugin, PluginKey } from "@tiptap/pm/state";
 
+type TransactionBatchPluginState = {
+    lastHandledTransactionKey: string | null;
+}
+
 function createTransactionBatchPlugin() {
     return new Plugin({
         key: new PluginKey("transactionBatchPlugin"),
+
+        state: {
+            init: (): TransactionBatchPluginState => ({ lastHandledTransactionKey: null }),
+            apply(transaction, value) {
+                
+            }
+        },
 
         appendTransaction(transactions, oldState, newState) {
             const docChanges = transactions.filter(transaction => transaction.docChanged);
