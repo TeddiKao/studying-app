c85a0efd86f9eb61113840343d7acf8f06f5d654
Teddi <teddikao@gmail.com>
2025-11-03 20:01:29 +0800

    Add hover styling to mark buttons

--- app/src/features/editor/components/EditorBubbleMenu.tsx
+++ app/src/features/editor/components/EditorBubbleMenu.tsx
@@ -15,15 +15,15 @@ function EditorBubbleMenu({ editor }: EditorBubbleMenuProps) {
 	return (
 		<BubbleMenu className="bg-white shadow-md rounded-md" editor={editor}>
 			<div className="flex flex-row gap-2">
-				<Button className="bg-white" type="button">
+				<Button className="bg-white hover:bg-gray-300" type="button">
                     <Bold className="stroke-gray-950" />
                 </Button>
 
-                <Button className="bg-white" type="button">
+                <Button className="bg-white hover:bg-gray-300" type="button">
                     <Italic className="stroke-gray-950" />
                 </Button>
 
-                <Button className="bg-white" type="button">
+                <Button className="bg-white hover:bg-gray-300" type="button">
                     <Underline className="stroke-gray-950" />
                 </Button>
 			</div>
