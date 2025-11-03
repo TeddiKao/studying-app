a443d3c93f1254ab1e4235b936147e4f595787a1
Teddi <teddikao@gmail.com>
2025-11-03 20:10:28 +0800

    Wrap mark buttons in a div

--- app/src/features/editor/components/EditorBubbleMenu.tsx
+++ app/src/features/editor/components/EditorBubbleMenu.tsx
@@ -6,26 +6,40 @@ import { BubbleMenu } from "@tiptap/react/menus";
 import { Bold, Italic, Underline } from "lucide-react";
 
 type EditorBubbleMenuProps = {
-    editor: Editor | null;
+	editor: Editor | null;
 };
 
 function EditorBubbleMenu({ editor }: EditorBubbleMenuProps) {
-    if (!editor) return null;
+	if (!editor) return null;
 
 	return (
-		<BubbleMenu className="bg-white shadow-md rounded-md p-1" editor={editor}>
+		<BubbleMenu
+			className="bg-white shadow-md rounded-md p-1"
+			editor={editor}
+		>
 			<div className="flex flex-row">
-				<button className="bg-white hover:bg-gray-300 p-1.5 rounded-md" type="button">
-                    <Bold className="stroke-gray-950 h-4 w-4" />
-                </button>
+				<div className="flex flex-row">
+					<button
+						className="bg-white hover:bg-gray-300 p-1.5 rounded-md"
+						type="button"
+					>
+						<Bold className="stroke-gray-950 h-4 w-4" />
+					</button>
 
-                <button className="bg-white hover:bg-gray-300 p-1.5 rounded-md" type="button">
-                    <Italic className="stroke-gray-950 h-4 w-4" />
-                </button>
+					<button
+						className="bg-white hover:bg-gray-300 p-1.5 rounded-md"
+						type="button"
+					>
+						<Italic className="stroke-gray-950 h-4 w-4" />
+					</button>
 
-                <button className="bg-white hover:bg-gray-300 p-1.5 rounded-md" type="button">
-                    <Underline className="stroke-gray-950 h-4 w-4" />
-                </button>
+					<button
+						className="bg-white hover:bg-gray-300 p-1.5 rounded-md"
+						type="button"
+					>
+						<Underline className="stroke-gray-950 h-4 w-4" />
+					</button>
+				</div>
 			</div>
 		</BubbleMenu>
 	);
