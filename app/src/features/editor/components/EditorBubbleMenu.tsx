f68216548e8fa4a14b964eef65015088ebd7fd0d
Teddi <teddikao@gmail.com>
2025-11-04 07:45:03 +0800

    Extract mark button to a reusable component

--- app/src/features/editor/components/EditorBubbleMenu.tsx
+++ app/src/features/editor/components/EditorBubbleMenu.tsx
@@ -15,6 +15,39 @@ type EditorBubbleMenuProps = {
 	editor: Editor | null;
 };
 
+type BubbleMenuMarkButtonProps = {
+	editor: Editor;
+	mark: "bold" | "italic" | "underline";
+};
+
+function BubbleMenuMarkButton({ editor, mark }: BubbleMenuMarkButtonProps) {
+	function handleMarkButtonClick() {
+		if (mark === "bold") {
+			editor.chain().focus().toggleBold().run();
+		} else if (mark === "italic") {
+			editor.chain().focus().toggleItalic().run();
+		} else if (mark === "underline") {
+			editor.chain().focus().toggleUnderline().run();
+		}
+	}
+
+	const iconMap = new Map<string, React.ReactNode>([
+		["bold", <Bold className="stroke-gray-950 h-4 w-4" />],
+		["italic", <Italic className="stroke-gray-950 h-4 w-4" />],
+		["underline", <Underline className="stroke-gray-950 h-4 w-4" />],
+	])
+
+	return (
+		<button
+			onClick={handleMarkButtonClick}
+			className="bg-white hover:bg-gray-300 p-1.5 rounded-md"
+			type="button"
+		>
+			{iconMap.get(mark)}
+		</button>
+	);
+}
+
 function EditorBubbleMenu({ editor }: EditorBubbleMenuProps) {
 	if (!editor) return null;
 
@@ -26,7 +59,9 @@ function EditorBubbleMenu({ editor }: EditorBubbleMenuProps) {
 			<div className="flex flex-row">
 				<div className="flex flex-row">
 					<button
-						onClick={() => editor.chain().focus().toggleBold().run()}
+						onClick={() =>
+							editor.chain().focus().toggleBold().run()
+						}
 						className="bg-white hover:bg-gray-300 p-1.5 rounded-md"
 						type="button"
 					>
@@ -34,7 +69,9 @@ function EditorBubbleMenu({ editor }: EditorBubbleMenuProps) {
 					</button>
 
 					<button
-						onClick={() => editor.chain().focus().toggleItalic().run()}
+						onClick={() =>
+							editor.chain().focus().toggleItalic().run()
+						}
 						className="bg-white hover:bg-gray-300 p-1.5 rounded-md"
 						type="button"
 					>
@@ -42,7 +79,9 @@ function EditorBubbleMenu({ editor }: EditorBubbleMenuProps) {
 					</button>
 
 					<button
-						onClick={() => editor.chain().focus().toggleUnderline().run()}
+						onClick={() =>
+							editor.chain().focus().toggleUnderline().run()
+						}
 						className="bg-white hover:bg-gray-300 p-1.5 rounded-md"
 						type="button"
 					>
@@ -59,15 +98,20 @@ function EditorBubbleMenu({ editor }: EditorBubbleMenuProps) {
 						<PopoverTrigger asChild>
 							<button className="flex flex-row gap-2 items-center text-left rounded px-2 py-1 text-sm">
 								<span>Paragraph</span>
-                                <ChevronDown className="w-5 h-5 stroke-gray-950" />
+								<ChevronDown className="w-5 h-5 stroke-gray-950" />
 							</button>
 						</PopoverTrigger>
 
-						<PopoverContent align="start" alignOffset={-8} side="bottom" className="w-[140px] p-1 border-none">
+						<PopoverContent
+							align="start"
+							alignOffset={-8}
+							side="bottom"
+							className="w-[140px] p-1 border-none"
+						>
 							<MenuItem onClick={() => {}}>Heading 1</MenuItem>
-                            <MenuItem onClick={() => {}}>Heading 2</MenuItem>
-                            <MenuItem onClick={() => {}}>Heading 3</MenuItem>
-                            <MenuItem onClick={() => {}}>Paragraph</MenuItem>
+							<MenuItem onClick={() => {}}>Heading 2</MenuItem>
+							<MenuItem onClick={() => {}}>Heading 3</MenuItem>
+							<MenuItem onClick={() => {}}>Paragraph</MenuItem>
 						</PopoverContent>
 					</Popover>
 				</div>
