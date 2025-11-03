2f757a653e8e0e303b8ae16c76fde653f05b0aa3
Teddi <teddikao@gmail.com>
2025-11-03 19:47:04 +0800

    Add placeholder bubble menu component

--- app/src/features/editor/components/EditorBubbleMenu.tsx
+++ app/src/features/editor/components/EditorBubbleMenu.tsx
@@ -1,5 +1,13 @@
-function EditorBubbleMenu() {
+"use client";
 
+import { BubbleMenu } from "@tiptap/react/menus";
+
+function EditorBubbleMenu() {
+    return (
+        <BubbleMenu>
+            
+        </BubbleMenu>
+    )
 }
 
 export default EditorBubbleMenu;
