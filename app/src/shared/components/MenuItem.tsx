768ed0defd1636f539bafb3beb2d1561887954b0
Teddi <teddikao@gmail.com>
2025-11-03 20:43:24 +0800

    Create custom MenuItem component

--- app/src/shared/components/MenuItem.tsx
+++ app/src/shared/components/MenuItem.tsx
@@ -1,5 +1,19 @@
-function MenuItem() {
-    
+type MenuItemProps = {
+    onClick: () => void;
+    children: React.ReactNode;
+}
+
+function MenuItem({ onClick, children }: MenuItemProps) {
+	return (
+		<button
+			onClick={onClick}
+			className="w-full rounded-sm px-2 py-1.5 text-sm outline-none 
+                 hover:bg-accent hover:text-accent-foreground
+                 focus:bg-accent focus:text-accent-foreground"
+		>
+			{children}
+		</button>
+	);
 }
 
 export default MenuItem;
