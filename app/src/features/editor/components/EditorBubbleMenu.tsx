02bab5ec5f7c714b61232388d0dab42959157e33
Teddi <teddikao@gmail.com>
2025-11-03 20:05:12 +0800

    Decrease padding of buttons

--- app/src/features/editor/components/EditorBubbleMenu.tsx
+++ app/src/features/editor/components/EditorBubbleMenu.tsx
@@ -15,15 +15,15 @@ function EditorBubbleMenu({ editor }: EditorBubbleMenuProps) {
 	return (
 		<BubbleMenu className="bg-white shadow-md rounded-md p-1" editor={editor}>
 			<div className="flex flex-row">
-				<Button className="bg-white hover:bg-gray-300" type="button">
+				<Button className="bg-white hover:bg-gray-300 p-1" type="button">
                     <Bold className="stroke-gray-950" />
                 </Button>
 
-                <Button className="bg-white hover:bg-gray-300" type="button">
+                <Button className="bg-white hover:bg-gray-300 p-1" type="button">
                     <Italic className="stroke-gray-950" />
                 </Button>
 
-                <Button className="bg-white hover:bg-gray-300" type="button">
+                <Button className="bg-white hover:bg-gray-300 p-1" type="button">
                     <Underline className="stroke-gray-950" />
                 </Button>
 			</div>
