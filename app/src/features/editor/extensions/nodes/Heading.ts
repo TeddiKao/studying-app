be628a1363565e7a17bc174fa136284bbdcc43db
Teddi <teddikao@gmail.com>
2025-11-04 13:27:06 +0800

    Ensure that heading node is exported

--- app/src/features/editor/extensions/nodes/Heading.ts
+++ app/src/features/editor/extensions/nodes/Heading.ts
@@ -1,30 +1,32 @@
 import { Heading } from "@tiptap/extension-heading";
 
 const CustomHeading = Heading.extend({
-    addOptions() {
-        return {
-            ...this.parent?.(),
-            levels: [1, 2, 3],
-            HTMLAttributes: {
-                class: "mb-4",
-            }
-        }
-    },
+	addOptions() {
+		return {
+			...this.parent?.(),
+			levels: [1, 2, 3],
+			HTMLAttributes: {
+				class: "mb-4",
+			},
+		};
+	},
 
-    addAttributes() {
-        return {
-            ...this.parent?.(),
-            
-            id: {
-                default: null,
-                rendered: false,
-                keepOnSplit: false,
-            },
-            position: {
-                default: null,
-                rendered: false,
-                keepOnSplit: false,
-            },
-        }
-    }
-})
+	addAttributes() {
+		return {
+			...this.parent?.(),
+
+			id: {
+				default: null,
+				rendered: false,
+				keepOnSplit: false,
+			},
+			position: {
+				default: null,
+				rendered: false,
+				keepOnSplit: false,
+			},
+		};
+	},
+});
+
+export { CustomHeading };
