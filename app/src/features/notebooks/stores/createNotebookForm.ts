d8c2e133d3a1d76617d6571164d057155f35c5b3
Teddi <teddikao@gmail.com>
2025-11-05 15:19:26 +0800

    Create store for managing errors when creating a notebook

--- app/src/features/notebooks/stores/createNotebookForm.ts
+++ app/src/features/notebooks/stores/createNotebookForm.ts
@@ -1,45 +1,80 @@
 import { create } from "zustand";
 
 type CreateNotebookFormState = {
-    isOpen: boolean;
-    isSubmitting: boolean;
-    
-    name: string;
-    description: string;
+	isOpen: boolean;
+	isSubmitting: boolean;
 
-    openForm: () => void;
-    closeForm: () => void;
+	name: string;
+	description: string;
 
-    updateName: (name: string) => void;
-    updateDescription: (description: string) => void;
-    clearName: () => void;
-    clearDescription: () => void;
+	openForm: () => void;
+	closeForm: () => void;
 
-    startSubmitting: () => void;
-    stopSubmitting: () => void;
+	updateName: (name: string) => void;
+	updateDescription: (description: string) => void;
+	clearName: () => void;
+	clearDescription: () => void;
 
-    performFormCleanup: () => void;
-}
+	startSubmitting: () => void;
+	stopSubmitting: () => void;
+
+	performFormCleanup: () => void;
+};
+
+type CreateNotebookFormErrorStore = {
+	general: string[];
+	name: string[];
+	description: string[];
+
+	updateGeneralErrors: (errors: string[]) => void;
+	updateNameErrors: (errors: string[]) => void;
+	updateDescriptionErrors: (errors: string[]) => void;
+
+	clearGeneralErrors: () => void;
+	clearNameErrors: () => void;
+	clearDescriptionErrors: () => void;
+
+	clearAllErrors: () => void;
+};
 
 const useCreateNotebookFormStore = create<CreateNotebookFormState>((set) => ({
-    isOpen: false,
-    isSubmitting: false,
-    name: "",
-    description: "",
+	isOpen: false,
+	isSubmitting: false,
+	name: "",
+	description: "",
 
-    updateName: (name: string) => set({ name }),
-    updateDescription: (description: string) => set({ description }),
+	updateName: (name: string) => set({ name }),
+	updateDescription: (description: string) => set({ description }),
 
-    clearName: () => set({ name: "" }),
-    clearDescription: () => set({ description: "" }),
+	clearName: () => set({ name: "" }),
+	clearDescription: () => set({ description: "" }),
 
-    openForm: () => set({ isOpen: true }),
-    closeForm: () => set({ isOpen: false }),
+	openForm: () => set({ isOpen: true }),
+	closeForm: () => set({ isOpen: false }),
 
-    startSubmitting: () => set({ isSubmitting: true }),
-    stopSubmitting: () => set({ isSubmitting: false }),
+	startSubmitting: () => set({ isSubmitting: true }),
+	stopSubmitting: () => set({ isSubmitting: false }),
 
-    performFormCleanup: () => set({ name: "", description: "", isOpen: false }),
+	performFormCleanup: () => set({ name: "", description: "", isOpen: false }),
 }));
 
-export { useCreateNotebookFormStore };
+const useCreateNotebookFormErrorStore = create<CreateNotebookFormErrorStore>(
+	(set) => ({
+		general: [],
+		name: [],
+		description: [],
+
+		updateGeneralErrors: (errors: string[]) => set({ general: errors }),
+		updateNameErrors: (errors: string[]) => set({ name: errors }),
+		updateDescriptionErrors: (errors: string[]) =>
+			set({ description: errors }),
+
+		clearGeneralErrors: () => set({ general: [] }),
+		clearNameErrors: () => set({ name: [] }),
+		clearDescriptionErrors: () => set({ description: [] }),
+
+		clearAllErrors: () => set({ general: [], name: [], description: [] }),
+	})
+);
+
+export { useCreateNotebookFormStore, useCreateNotebookFormErrorStore };
