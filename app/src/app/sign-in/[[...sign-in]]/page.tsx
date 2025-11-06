ee95959a5e5764cc1619098235b46acd285b09d4
Teddi <teddikao@gmail.com>
2025-11-06 17:42:14 +0800

    Add notice at bottom of sign in page

--- app/src/app/sign-in/[[...sign-in]]/page.tsx
+++ app/src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,11 +1,19 @@
 import { SignIn } from "@clerk/nextjs";
 
 function SignInPage() {
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen w-full">
-            <SignIn />
-        </div>
-    )
+	return (
+		<div className="flex flex-col items-center justify-center min-h-screen w-full">
+			<SignIn />
+			<div className="flex flex-col items-center justify-center mt-4 text-sm">
+				<span>
+					<span>By signing up or logging in, you agree to our </span>
+					<a href="/terms" className="text-gray-500 font-bold">
+						Terms of Service.
+					</a>
+				</span>
+			</div>
+		</div>
+	);
 }
 
 export default SignInPage;
