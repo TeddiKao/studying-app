e4387a5ec0c1f27b36a538078e5b7c148951f9c1
Teddi <teddikao@gmail.com>
2025-11-12 10:22:29 +0800

    Modify href of GitHub link accordingly

--- app/src/app/(home)/page.tsx
+++ app/src/app/(home)/page.tsx
@@ -1,3 +1,4 @@
+import { GITHUB_REPO_URL } from "@/shared/constants/constants";
 import Link from "next/link";
 
 export default function Home() {
@@ -5,7 +6,7 @@ export default function Home() {
 		<div className="flex flex-col p-4 w-full">
 			<nav className="flex flex-row gap-6 items-center justify-center w-full">
 				<Link href={"/"}>Home</Link>
-				<Link href={"/"}>GitHub</Link>
+				<Link href={GITHUB_REPO_URL}>GitHub</Link>
 			</nav>
 		</div>
 	);
