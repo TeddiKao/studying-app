e671e3b990492345e390b6d7693794ea6315d10e
Teddi <teddikao@gmail.com>
2025-11-12 10:21:00 +0800

    Create placeholder link to GitHub repository

--- app/src/app/(home)/page.tsx
+++ app/src/app/(home)/page.tsx
@@ -2,9 +2,10 @@ import Link from "next/link";
 
 export default function Home() {
 	return (
-		<div className="flex flex-col w-full">
-			<nav className="flex flex-row items-center justify-center w-full">
+		<div className="flex flex-col p-4 w-full">
+			<nav className="flex flex-row gap-6 items-center justify-center w-full">
 				<Link href={"/"}>Home</Link>
+				<Link href={"/"}>GitHub</Link>
 			</nav>
 		</div>
 	);
