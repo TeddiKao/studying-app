c3206ffa2c8c9880b565a94b1bceec7e2a661e4f
Teddi <teddikao@gmail.com>
2025-11-12 11:11:45 +0800

    Remove empty spacer and use grid instead

--- app/src/app/(home)/page.tsx
+++ app/src/app/(home)/page.tsx
@@ -5,17 +5,15 @@ import Link from "next/link";
 export default function Home() {
 	return (
 		<div className="flex flex-col p-4 w-full">
-			<nav className="flex flex-row items-center justify-between w-full text-sm">
-				<div></div>
-				
-				<div className="flex flex-row gap-6 items-center">
+			<nav className="grid grid-cols-3 items-center w-full text-sm">
+				<div className="flex flex-row gap-6 items-center justify-center col-start-2">
 					<Link href={"/"}>Home</Link>
 					<Link rel="noopener noreferrer" target="_blank" href={GITHUB_REPO_URL}>
 						GitHub
 					</Link>
 				</div>
 
-				<div className="flex flex-row items-center">
+				<div className="flex flex-row items-center justify-end">
 					<Button asChild>
 						<Link href="/sign-in">Sign in</Link>
 					</Button>
