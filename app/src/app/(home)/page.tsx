599cc7a2118709abffd27419787c6c1708242a1f
Teddi <teddikao@gmail.com>
2025-11-12 10:35:44 +0800

    Improve styling of home page

--- app/src/app/(home)/page.tsx
+++ app/src/app/(home)/page.tsx
@@ -1,15 +1,24 @@
+import { Button } from "@/components/ui/button";
 import { GITHUB_REPO_URL } from "@/shared/constants/constants";
 import Link from "next/link";
 
 export default function Home() {
 	return (
-		<div className="flex flex-col p-6 w-full">
-			<nav className="flex flex-row items-center justify-center w-full text-sm">
-				<div className="flex flex-row gap-6">
+		<div className="flex flex-col p-4 w-full">
+			<nav className="flex flex-row items-center justify-between w-full text-sm">
+				<div></div>
+				
+				<div className="flex flex-row gap-6 items-center">
 					<Link href={"/"}>Home</Link>
 					<Link target="_blank" href={GITHUB_REPO_URL}>
 						GitHub
 					</Link>
+				</div>
+
+				<div className="flex flex-row items-center">
+					<Button asChild>
+						<Link href="/sign-in">Sign in</Link>
+					</Button>
 				</div>
 			</nav>
 		</div>
