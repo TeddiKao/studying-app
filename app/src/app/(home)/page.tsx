ad273732f1859514089abe35044ef692e949b265
Teddi <teddikao@gmail.com>
2025-11-12 10:17:33 +0800

    Add basic markup for home page

--- app/src/app/(home)/page.tsx
+++ app/src/app/(home)/page.tsx
@@ -1,5 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
 	return (
-		<div>Home</div>
+		<div className="flex flex-col w-full">
+			<nav className="flex flex-row items-center justify-center w-full">
+				<Link href={"/"}>Home</Link>
+			</nav>
+		</div>
 	);
 }
