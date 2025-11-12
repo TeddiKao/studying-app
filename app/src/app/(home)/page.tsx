a0058b738555313b91db9a23676ed82873360305
Teddi <teddikao@gmail.com>
2025-11-12 10:24:39 +0800

    Decrease size of nav links

--- app/src/app/(home)/page.tsx
+++ app/src/app/(home)/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 export default function Home() {
 	return (
-		<div className="flex flex-col p-4 w-full">
-			<nav className="flex flex-row gap-6 items-center justify-center w-full">
+		<div className="flex flex-col p-6 w-full">
+			<nav className="flex flex-row gap-6 items-center justify-center w-full text-sm">
 				<Link href={"/"}>Home</Link>
 				<Link target="_blank" href={GITHUB_REPO_URL}>GitHub</Link>
 			</nav>
