1bd39b89a75427f435ebcac287ccfdef582bbe70
Teddi <teddikao@gmail.com>
2025-12-11 18:16:12 +0800

    Create store to manage file upload box state to store preview image

---
+++ app/src/features/notes/stores/uploadBox.ts
@@ -0,0 +1,15 @@
+import { create } from "zustand";
+
+type FileUploadBoxStore = {
+	previewImageUrl: string | null;
+
+	setPreviewImageUrl: (url: string) => void;
+	clearPreviewImageUrl: () => void;
+};
+
+const useFileUploadBoxStore = create<FileUploadBoxStore>((set) => ({
+	previewImageUrl: null,
+
+	setPreviewImageUrl: (url: string) => set({ previewImageUrl: url }),
+	clearPreviewImageUrl: () => set({ previewImageUrl: null }),
+}));
