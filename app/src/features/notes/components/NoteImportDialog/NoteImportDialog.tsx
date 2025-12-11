e9b18b7a146bf2898931f423c9d8af5ef3fed5e5
Teddi <teddikao@gmail.com>
2025-12-11 17:16:30 +0800

    Add open and onOpenChange props to integrate state with dialog visiblity

--- app/src/features/notes/components/NoteImportDialog/NoteImportDialog.tsx
+++ app/src/features/notes/components/NoteImportDialog/NoteImportDialog.tsx
@@ -1,4 +1,4 @@
-import { DialogHeader } from "@/components/ui/dialog";
+import { Dialog, DialogHeader } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
@@ -6,29 +6,42 @@ import {
 	DialogDescription,
 	DialogTitle,
 } from "@radix-ui/react-dialog";
+import { useNoteImportDialogStore } from "../../stores/noteImportDialog";
 
 function NoteImportDialog() {
+	const { isOpen, openDialog, closeDialog } = useNoteImportDialogStore();
+
 	return (
-		<DialogContent>
-			<DialogHeader>
-				<DialogTitle>Import note</DialogTitle>
-				<DialogDescription>
-					Import a handwritten note. More options like uploading files
-					and importing from third-party apps will be coming soon.
-					soon.
-				</DialogDescription>
-			</DialogHeader>
+		<Dialog open={isOpen} onOpenChange={(open) => {
+			if (open) {
+				openDialog();
+			} else {
+				closeDialog();
+			}
+		}}>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle>Import note</DialogTitle>
+					<DialogDescription>
+						Import a handwritten note. More options like uploading
+						files and importing from third-party apps will be coming
+						soon. soon.
+					</DialogDescription>
+				</DialogHeader>
 
-			<Tabs>
-				<TabsList>
-					<TabsTrigger value="uploadFile">Upload file</TabsTrigger>
-				</TabsList>
+				<Tabs>
+					<TabsList>
+						<TabsTrigger value="uploadFile">
+							Upload file
+						</TabsTrigger>
+					</TabsList>
 
-				<TabsContent value="handwriting">
-					<Input type="file" />
-				</TabsContent>
-			</Tabs>
-		</DialogContent>
+					<TabsContent value="handwriting">
+						<Input type="file" />
+					</TabsContent>
+				</Tabs>
+			</DialogContent>
+		</Dialog>
 	);
 }
 
