0be44b1b258898dea60d80b6919660e0a11a8b78
Teddi <teddikao@gmail.com>
2025-12-11 18:17:55 +0800

    Rename store key for clarity

--- app/src/features/notes/stores/uploadBox.ts
+++ app/src/features/notes/stores/uploadBox.ts
@@ -3,14 +3,14 @@ import { create } from "zustand";
 type FileUploadBoxStore = {
 	previewImageUrl: string | null;
 
-	setPreviewImageUrl: (url: string) => void;
+	updatePreviewImageUrl: (url: string) => void;
 	clearPreviewImageUrl: () => void;
 };
 
 const useFileUploadBoxStore = create<FileUploadBoxStore>((set) => ({
 	previewImageUrl: null,
 
-	setPreviewImageUrl: (url: string) => set({ previewImageUrl: url }),
+	updatePreviewImageUrl: (url: string) => set({ previewImageUrl: url }),
 	clearPreviewImageUrl: () => set({ previewImageUrl: null }),
 }));
 
