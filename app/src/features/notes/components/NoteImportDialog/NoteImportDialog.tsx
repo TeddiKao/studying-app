056a2e0f564f352cb03488427dccab17db6b107b
Teddi <teddikao@gmail.com>
2025-12-11 18:03:42 +0800

    Create file upload box

--- app/src/features/notes/components/NoteImportDialog/NoteImportDialog.tsx
+++ app/src/features/notes/components/NoteImportDialog/NoteImportDialog.tsx
@@ -1,24 +1,29 @@
-import { Dialog, DialogHeader } from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
+	Dialog,
 	DialogContent,
 	DialogDescription,
+	DialogHeader,
 	DialogTitle,
-} from "@radix-ui/react-dialog";
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useNoteImportDialogStore } from "../../stores/noteImportDialog";
+import { FileIcon, ImageIcon } from "lucide-react";
 
 function NoteImportDialog() {
 	const { isOpen, openDialog, closeDialog } = useNoteImportDialogStore();
 
 	return (
-		<Dialog open={isOpen} onOpenChange={(open) => {
-			if (open) {
-				openDialog();
-			} else {
-				closeDialog();
-			}
-		}}>
+		<Dialog
+			open={isOpen}
+			onOpenChange={(open) => {
+				if (open) {
+					openDialog();
+				} else {
+					closeDialog();
+				}
+			}}
+		>
 			<DialogContent>
 				<DialogHeader>
 					<DialogTitle>Import note</DialogTitle>
@@ -30,16 +35,23 @@ function NoteImportDialog() {
 				</DialogHeader>
 
 				<Tabs>
-					<TabsList>
-						<TabsTrigger value="uploadFile">
+					<TabsList className="bg-transparent">
+						<TabsTrigger
+							className="rounded-none bg-transparent h-full data-[state=active]:shadow-none border-b-2 border-transparent data-[state=active]:border-primary"
+							value="uploadFile"
+						>
 							Upload file
 						</TabsTrigger>
 					</TabsList>
 
-					<TabsContent value="handwriting">
-						<Input type="file" />
+					<TabsContent value="uploadFile">
+						<div className="flex flex-col gap-2 hover:cursor-pointer items-center justify-center bg-gray-200 w-full rounded-lg py-6">
+							<FileIcon className="size-12 stroke-gray-500" />
+							<p className="text-sm text-gray-500">Upload an image of your handwritten note</p>
+						</div>
 					</TabsContent>
 				</Tabs>
+				
 			</DialogContent>
 		</Dialog>
 	);
