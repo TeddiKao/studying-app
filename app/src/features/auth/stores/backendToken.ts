a5c704678d21b2255c43db3ed5acfa89c703b19b
Teddi <teddikao@gmail.com>
2025-12-13 12:59:42 +0800

    Add store function for clearing token getter

--- app/src/features/auth/stores/backendToken.ts
+++ app/src/features/auth/stores/backendToken.ts
@@ -3,12 +3,16 @@ import { create } from "zustand";
 type BackendVerificationTokenManagerStore = {
 	getToken: (() => Promise<string | null>) | null;
 	updateTokenGetter: (tokenGetter: () => Promise<string | null>) => void;
+	clearTokenGetter: () => void;
 }
 
 const useBackendVerificationTokenStore = create<BackendVerificationTokenManagerStore>((set) => ({
 	getToken: null,
 	updateTokenGetter: (tokenGetter) => {
 		set({ getToken: tokenGetter })
+	},
+	clearTokenGetter: () => {
+		set({ getToken: null })
 	}
 }))
 
