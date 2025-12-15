15d283e6bfc64e07a0e11edf28bab1d86c3ecdd8
Teddi <teddikao@gmail.com>
2025-12-15 20:44:30 +0800

    Create function for extracting text from image using built in model

---
+++ ai_services/models/ocr_model.py
@@ -0,0 +1,21 @@
+from fastapi import UploadFile
+from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+from PIL import Image
+from io import BytesIO
+
+import os
+print("CWD:", os.getcwd())
+
+processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-handwritten")
+model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-base-handwritten")
+
+async def get_text_from_image(image_file: UploadFile = File(...)):
+	image_contents = await image_file.read()
+	image = Image.open(BytesIO(image_contents)).convert("RGB")
+
+	pixel_values = processor(images=image, return_tensors="pt").pixel_values
+	generated_ids = model.generate(pixel_values)
+
+	text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+	
+	return text
