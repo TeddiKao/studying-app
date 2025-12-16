16266ae1e50d28decc5eff93bf118d72348f70cd
Teddi <teddikao@gmail.com>
2025-12-16 18:47:22 +0800

    Integrate setup function with main.py file

--- ai_services/main.py
+++ ai_services/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 
 from .routers import note_upload_service
+from .middleware import add_cors_middleware
 
 app = FastAPI(root_path="/api")
+
+add_cors_middleware(app)
 
 app.include_router(note_upload_service.router)
