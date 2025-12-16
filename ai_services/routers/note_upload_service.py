8507bb27c47fbcda8bdadfd171de9588abe871b0
Teddi <teddikao@gmail.com>
2025-12-16 19:38:25 +0800

    Add prefix

--- ai_services/routers/note_upload_service.py
+++ ai_services/routers/note_upload_service.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request, Header, APIRouter
 
-router = APIRouter()
+router = APIRouter(prefix="/notes")
 
 @router.post("/upload_note")
 async def upload_note(authorization: str = Header(None)):
