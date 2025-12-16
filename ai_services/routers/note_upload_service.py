75cab50c8aa2d1cf5cfbe61bd38a77cafefc3671
Teddi <teddikao@gmail.com>
2025-12-16 19:01:09 +0800

    Remove print statement and instead return a response

--- ai_services/routers/note_upload_service.py
+++ ai_services/routers/note_upload_service.py
@@ -4,4 +4,7 @@ router = APIRouter()
 
 @router.post("/upload_note")
 async def upload_note(authorization: str = Header(None)):
-	print(authorization)
+	return {
+		"success": True,
+		"token": authorization
+	}
