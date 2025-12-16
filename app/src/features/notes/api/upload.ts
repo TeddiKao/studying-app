e5091c8656417f725b2b164aaba57f96d4de80b7
Teddi <teddikao@gmail.com>
2025-12-16 19:38:53 +0800

    Update URL accordingly

--- app/src/features/notes/api/upload.ts
+++ app/src/features/notes/api/upload.ts
@@ -2,7 +2,7 @@ import api from "@/lib/axiosApi";
 
 async function sendFileToBackendForNoteImport(file: string) {
 	try {
-		const response = await api.post("upload_note");
+		const response = await api.post("notes/upload_note");
 		console.log(response);
 	} catch (error) {
 		console.error(error);
