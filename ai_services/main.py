b10f46ede4a7f08000a221214a605e51f776cbdf
Teddi <teddikao@gmail.com>
2025-12-19 18:24:09 +0800

    Add dependency to routes

--- ai_services/main.py
+++ ai_services/main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 
 from .routers import note_upload_service
 from .middleware import add_cors_middleware
+from .dependencies import verify_jwt_token_in_header
 
-app = FastAPI(root_path="/api")
+app = FastAPI(root_path="/api", dependencies=[Depends(verify_jwt_token_in_header)])
 
 add_cors_middleware(app)
 
