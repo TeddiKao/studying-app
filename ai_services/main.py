a97505d04dae92f23c945ff4a74301a8302b4e37
Teddi <teddikao@gmail.com>
2026-01-09 11:15:32 +0800

    Add auth dependency to app

--- ai_services/main.py
+++ ai_services/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, Depends
 
-from .routers import note_upload_service
-from .middleware import add_cors_middleware
-from .dependencies import verify_jwt_token_in_header
+from ai_services.routers import note_upload_service
+from ai_services.middleware import add_cors_middleware
+from ai_services.dependencies import verify_jwt_token_in_header
 
 app = FastAPI(root_path="/api", dependencies=[Depends(verify_jwt_token_in_header)])
 
