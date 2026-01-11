593d49d53d78457c0c1c319240e41402e8e97eca
Teddi <teddikao@gmail.com>
2026-01-11 16:38:32 +0800

    Add timeout to request

--- ai_services/dependencies.py
+++ ai_services/dependencies.py
@@ -23,7 +23,7 @@ def get_jwks() -> Dict[str, Any]:
 			raise RuntimeError("CLERK_JWKS_ENDPOINT environment variable is not set")
 
 		response = requests.get(jwks_endpoint)
-		response.raise_for_status()
+		response.raise_for_status(timeout=10)
 		jwks_cache = response.json()
 
 	return jwks_cache
