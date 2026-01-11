3d75b3025bb950a5a52bd244678088f41c3cdc7c
Teddi <teddikao@gmail.com>
2026-01-11 15:26:23 +0800

    Increase API timeout

--- app/src/features/notes/api/upload.ts
+++ app/src/features/notes/api/upload.ts
@@ -3,7 +3,7 @@ import api from "@/lib/axiosApi";
 async function sendFileToBackendForNoteImport(formData: FormData) {
 	try {
 		const response = await api.post("notes/upload_note", formData, {
-			timeout: 10000,
+			timeout: 20000,
 		});
 		console.log(response);
 	} catch (error) {
