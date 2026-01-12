d7c6b19f36d5241f68eae295e6cb7b5637089091
Teddi <teddikao@gmail.com>
2026-01-12 17:53:45 +0800

    Add missing await when detecting file type

--- ai_services/note_import/dispatcher/service.py
+++ ai_services/note_import/dispatcher/service.py
@@ -3,7 +3,7 @@ from ai_services.note_import.dispatcher.file_type_detector import detect_file_ty
 from ai_services.note_import.ocr.service import handle_note_import_by_ocr
 
 async def handle_note_import(file: UploadFile):
-	file_type = detect_file_type(file)
+	file_type = await detect_file_type(file)
 
 	if file_type == "image/jpeg" or file_type == "image/png" or file_type == "image/jpg":
 		return await handle_note_import_by_ocr(file)
