139f7e32d18fe93a8c26d8723cf836336cd7d0d9
Teddi <teddikao@gmail.com>
2026-01-12 16:51:59 +0800

    Route appropriate file types to OCR reading accordingly

--- ai_services/note_import/dispatcher/service.py
+++ ai_services/note_import/dispatcher/service.py
@@ -1,4 +1,9 @@
 from fastapi import UploadFile
+from ai_services.note_import.dispatcher.file_type_detector import detect_file_type
+from ai_services.note_import.ocr.service import handle_note_import_by_ocr
 
-def handle_note_import(file: UploadFile):
-	pass
+async def handle_note_import(file: UploadFile):
+	file_type = detect_file_type(file)
+
+	if file_type == "image/jpeg" or file_type == "image/png" or file_type == "image/jpg":
+		return await handle_note_import_by_ocr(file)
