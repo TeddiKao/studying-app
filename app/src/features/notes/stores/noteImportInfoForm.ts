1f750e5143173d7d586eb4dcc98e7674878a7128
Teddi <teddikao@gmail.com>
2026-01-13 14:41:22 +0800

    Create store to manage state for note import info form

---
+++ app/src/features/notes/stores/noteImportInfoForm.ts
@@ -0,0 +1,29 @@
+import { create } from "zustand";
+
+type NoteImportInfoFormStore = {
+	title: string;
+	updateTitle: (title: string) => void;
+	clearTitle: () => void;
+
+	description: string;
+	updateDescription: (description: string) => void;
+	clearDescription: () => void;
+
+	isSubmitting: boolean;
+	startSubmitting: () => void;
+	stopSubmitting: () => void;
+}
+
+const useNoteImportInfoFormStore = create<NoteImportInfoFormStore>((set) => ({
+	title: "",
+	updateTitle: (title: string) => set({ title }),
+	clearTitle: () => set({ title: "" }),
+
+	description: "",
+	updateDescription: (description: string) => set({ description }),
+	clearDescription: () => set({ description: "" }),
+
+	isSubmitting: false,
+	startSubmitting: () => set({ isSubmitting: true }),
+	stopSubmitting: () => set({ isSubmitting: false }),
+}))
